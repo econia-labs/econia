ef913fe464e750672855db0f01d36da3906bdbc9
alnoki <43892045+alnoki@users.noreply.github.com>
2022-04-22 15:20:07 -0700

    Complete Aptos "Your first Move Module" tutorial

---
+++ src/move/ultima/src/lib.rs
@@ -0,0 +1,4 @@
+// Taken from https://github.com/magnum6actual/move-baseline/blob/master/src/lib.rs
+
+#[cfg(test)]
+mod move_unit_tests;
---
+++ src/move/ultima/src/move_unit_tests.rs
@@ -0,0 +1,24 @@
+// From https://github.com/magnum6actual/move-baseline/blob/master/src/move_unit_tests.rs
+// Based off `aptos-core/aptos-move/framework/move_unit_test.rs`
+
+use aptos_vm::natives::aptos_natives;
+use move_cli::package::cli;
+use move_unit_test::UnitTestingConfig;
+use std::path::PathBuf;
+
+#[test]
+fn move_unit_tests() {
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    cli::run_move_unit_tests(
+        &path,
+        move_package::BuildConfig {
+            test_mode: true,
+            install_dir: Some(path.clone()),
+            ..Default::default()
+        },
+        UnitTestingConfig::default_with_bound(Some(100_000)),
+        aptos_natives(),
+        /* compute_coverage */ false,
+    )
+    .unwrap();
+}

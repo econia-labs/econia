b728094609034368b112bf68cd71de36ffc5c81d
alnoki <43892045+alnoki@users.noreply.github.com>
2023-02-19 14:21:24 -0700

    Update incentives scripts, refactor for poetry

--- src/python/econia/incentives.py
+++ src/python/econia/incentives.py
@@ -1,186 +1,239 @@
 """Incentive parameters functionality."""
 
-from decimal import Decimal as dec
-from econia.defs import incentive_parameters as params, seps
+from decimal import Decimal
+from typing import List
+
+# Constants >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+MARKET_REGISTRATION_FEE = Decimal("25")
+"""USD Cost to register a market."""
+
+UNDERWRITER_REGISTRATION_FEE = Decimal("0.01")
+"""USD cost to register as an underwriter."""
+
+CUSTODIAN_REGISTRATION_FEE = Decimal("0.01")
+"""USD cost to register as a custodian."""
+
+TAKER_FEE_PERCENTAGE = Decimal("0.05")
+"""Flat taker fee percentage."""
+
+TIERS = [
+    [Decimal("0.01"), Decimal("0.00"), Decimal("0.20")],
+    [Decimal("0.012"), Decimal("0.20"), Decimal("0.19")],
+    [Decimal("0.013"), Decimal("3"), Decimal("0.18")],
+    [Decimal("0.014"), Decimal("40"), Decimal("0.17")],
+    [Decimal("0.015"), Decimal("500"), Decimal("0.16")],
+    [Decimal("0.016"), Decimal("6_000"), Decimal("0.15")],
+    [Decimal("0.017"), Decimal("70_000"), Decimal("0.14")],
+]
+"""Integrator tier parameters."""
+
+INDENT = "    "
+"""4-space indent."""
+
+# Constants <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
 
 def format_constant_definition(
     name: str,
-    amount: dec,
-    first_in_block: bool = False
+    amount: Decimal,
+    is_genesis_parameter: bool = False,
 ) -> str:
     """Format a constant definition for a block of constants.
 
-    Parameters
-    ----------
-    name : str
-        Constant name.
-    amount : decimal.Decimal
-        Constant amount.
-    first_in_block : bool, optional
-        If False, prepend a newline.
-
-    Returns
-    -------
-    str
-        Formatted constant definition.
-
     Example
     -------
-    >>> from decimal import Decimal as dec
-    >>> from econia.incentives import format_constant_definition
-    >>> print(format_constant_definition('MY_PARAMETER', dec('5.02'), True))
+    >>> print(format_constant_definition("FOO", Decimal("1.02"), True))
         /// Genesis parameter.
-        const MY_PARAMETER: u64 = 5;
-    >>> print(format_constant_definition('MY_PARAMETER', dec('5.02'), False))
-    <BLANKLINE>
-        /// Genesis parameter.
-        const MY_PARAMETER: u64 = 5;
+        const FOO: u64 = 1;
+    >>> print(format_constant_definition("BAR", Decimal("500")))
+        const BAR: u64 = 500;
     """
-    if first_in_block:
-        formatted = ''
-    else:
-        formatted = seps.nl
-    return formatted + params.doc_comment + params.indent \
-        + params.constant_token + seps.sp + name + seps.cln + seps.sp \
-        + params.constant_type + seps.sp + seps.eq + seps.sp \
-        + str(int(amount)) + seps.sc
+    # Prepend genesis parameter doc comment if necessary.
+    base = f"{INDENT}/// Genesis parameter.\n" if is_genesis_parameter else ""
+    # Return appended and formatted constant definition block.
+    return f"{base}{INDENT}const {name}: u64 = {str(int(amount))};"
 
-def percent_to_divisor(
-    percent: dec
-) -> int:
+
+def percent_to_divisor(percent: Decimal) -> int:
     """Convert a percent to an integer divisor.
 
-    Parameters
-    ----------
-    percent : str
-        A percent.
-
-    Returns
-    -------
-    int
-        Corresponding divisor.
-
     Example
     -------
-    >>> from decimal import Decimal as dec
-    >>> from econia.incentives import percent_to_divisor
-    >>> percent_to_divisor(dec('0.05'))
+    >>> percent_to_divisor(Decimal('0.05'))
     2000
-    >>> percent_to_divisor(dec('0.03'))
+    >>> percent_to_divisor(Decimal('0.03'))
     3333
-    >>> percent_to_divisor(dec('0.015'))
+    >>> percent_to_divisor(Decimal('0.015'))
     6667
     """
-    return int(round(1 / (percent / params.percent_base)))
+    return int(round(1 / (percent / 100)))
 
-def justify_constants(
-    block: str
-) -> str:
+
+def justify_constants(block: str) -> str:
     """Right justify all of the constants in a definition block.
-
-    Parameters
-    ----------
-    block : str
-        A block of text with Move constant definitions.
-
-    Returns
-    -------
-    str
-        The same block, with all constants right-justified.
 
     Example
     -------
-    >>> from econia.incentives import justify_constants
     >>> block = '''\\
-    ...     /// Comment 1.
+    ...     /// Short comment.
     ...     const CONSTANT_1: u64 = 1;
-    ...     /// Comment 2.
+    ...     /// Long comment that is longer than longest line of code.
     ...     const CONSTANT_2: u64 = 200;'''
     >>> print(justify_constants(block))
-        /// Comment 1.
+        /// Short comment.
         const CONSTANT_1: u64 =   1;
-        /// Comment 2.
+        /// Long comment that is longer than longest line of code.
         const CONSTANT_2: u64 = 200;
     """
-    lines = block.splitlines() # Get all lines.
-    max_length = 0 # Declare maximum line length.
-    for line in lines: # Loop over all lines.
-        if line[-1] is seps.sc: # If line ends in semicolon:
-            # Update maximum line length as needed.
-            if len(line) > max_length: max_length = len(line)
-    new_block = '' # Initialize new block.
-    for i, line in enumerate(lines): # Loop over all lines.
-        if i != 0: # If not on first line:
-            new_block = new_block + seps.nl # Append newline separator.
-        if line[-1] is seps.sc: # If line ends in semicolon:
-            indent = max_length - len(line) # Calculate indent required.
-            if indent != 0: # If need to indent line:
-                tokens = line.split() # Get tokens in line.
-                line = params.indent # Start line with indent.
-                while (len(tokens) > 1): # While multiple tokens left:
-                    # Append the next token to the line.
-                    line = line + tokens.pop(0) + seps.sp
-                # Add indenting spaces as needed, final token.
-                line = line + (indent) * seps.sp + tokens.pop()
-        new_block = new_block + line # Append line to block.
-    return new_block  # Return block.
+    lines = block.splitlines()  # Get all lines.
+    # Get maximum line length for lines of code (ending in semicolon).
+    max_length = max([len(line) for line in lines if line[-1] == ";"])
+    for i, line in enumerate(lines):  # Loop over all lines.
+        if line[-1] == ";":  # If line ends in semicolon (is code):
+            pad = (max_length - len(line)) * " "  # Get pad required.
+            if pad != "":  # If need to pad line:
+                tokens = line.split()  # Get tokens in line.
+                # Update line with padded integer value.
+                lines[i] = f"{INDENT}{' '.join(tokens[:-1])} {pad}{tokens[-1]}"
+    return "\n".join(lines)  # Return formatted block of new lines.
+
 
 def generate_constants_block(
-    utility_coin_usd_value: dec,
-    utility_coin_decimals: int
+    utility_coin_usd_value: Decimal,
+    utility_coin_decimals: int,
+    market_registration_fee: Decimal = MARKET_REGISTRATION_FEE,
+    underwriter_registration_fee: Decimal = UNDERWRITER_REGISTRATION_FEE,
+    custodian_registration_fee: Decimal = CUSTODIAN_REGISTRATION_FEE,
+    taker_fee_percentage: Decimal = TAKER_FEE_PERCENTAGE,
+    tiers: List[List[Decimal]] = TIERS,
+    is_genesis_block: bool = False,
 ) -> str:
-    """Generate a block of Move constant definitions.
+    """Return a block of Move incentive parameter constant definitions.
 
-    Parameters
-    ----------
-    utility_coin_usd_value : decimal.Decimal
-        Value of utility coin in US dollars.
-    utility_coin_decimals : int
-        Number of decimals in utility coin.
-
-    Returns
+    Example
     -------
-    str
-        Formatted block of Move constant definitions.
+    >>> utility_coin_usd_value = Decimal("20")
+    >>> utility_coin_decimals = 8
+    >>> market_registration_fee = Decimal("25")
+    >>> underwriter_registration_fee = Decimal("0.01")
+    >>> custodian_registration_fee = Decimal("0.01")
+    >>> taker_fee_percentage = Decimal("0.05")
+    >>> tiers = [
+    ...     [Decimal("0.01"), Decimal("0.00"), Decimal("0.20")],
+    ...     [Decimal("0.012"), Decimal("0.20"), Decimal("0.19")],
+    ... ]
+    >>> print(generate_constants_block(
+    ...     utility_coin_usd_value,
+    ...     utility_coin_decimals,
+    ...     market_registration_fee,
+    ...     underwriter_registration_fee,
+    ...     custodian_registration_fee,
+    ...     taker_fee_percentage,
+    ...     tiers))
+        const MARKET_REGISTRATION_FEE: u64 =  125000000;
+        const UNDERWRITER_REGISTRATION_FEE: u64 = 50000;
+        const CUSTODIAN_REGISTRATION_FEE: u64 =   50000;
+        const TAKER_FEE_DIVISOR: u64 =             2000;
+        const FEE_SHARE_DIVISOR_0: u64 =          10000;
+        const FEE_SHARE_DIVISOR_1: u64 =           8333;
+        const TIER_ACTIVATION_FEE_0: u64 =            0;
+        const TIER_ACTIVATION_FEE_1: u64 =      1000000;
+        const WITHDRAWAL_FEE_0: u64 =           1000000;
+        const WITHDRAWAL_FEE_1: u64 =            950000;
+    >>> print(generate_constants_block(
+    ...     utility_coin_usd_value,
+    ...     utility_coin_decimals,
+    ...     market_registration_fee,
+    ...     underwriter_registration_fee,
+    ...     custodian_registration_fee,
+    ...     taker_fee_percentage,
+    ...     tiers,
+    ...     is_genesis_block=True))
+        /// Genesis parameter.
+        const MARKET_REGISTRATION_FEE: u64 =  125000000;
+        /// Genesis parameter.
+        const UNDERWRITER_REGISTRATION_FEE: u64 = 50000;
+        /// Genesis parameter.
+        const CUSTODIAN_REGISTRATION_FEE: u64 =   50000;
+        /// Genesis parameter.
+        const TAKER_FEE_DIVISOR: u64 =             2000;
+        /// Genesis parameter.
+        const FEE_SHARE_DIVISOR_0: u64 =          10000;
+        /// Genesis parameter.
+        const FEE_SHARE_DIVISOR_1: u64 =           8333;
+        /// Genesis parameter.
+        const TIER_ACTIVATION_FEE_0: u64 =            0;
+        /// Genesis parameter.
+        const TIER_ACTIVATION_FEE_1: u64 =      1000000;
+        /// Genesis parameter.
+        const WITHDRAWAL_FEE_0: u64 =           1000000;
+        /// Genesis parameter.
+        const WITHDRAWAL_FEE_1: u64 =            950000;
     """
     # Get USD per subunit of utility coin.
-    usd_per_subunit = utility_coin_usd_value \
-        / dec(1).shift(utility_coin_decimals)
-    block = '' # Initialize empty block.
-    # Loop over registration fees:
-    for i, registration_fee in enumerate([params.market_registration_fee,
-                                          params.underwriter_registration_fee,
-                                          params.custodian_registration_fee]):
-        # Append constant definition.
-        block = block + format_constant_definition(
-            registration_fee.constant_name,
-            registration_fee.amount / usd_per_subunit,
-            i==0)
-    # Append taker fee divisor definition.
-    block = block + format_constant_definition(
-        params.taker_fee_percentage.constant_name,
-        percent_to_divisor(params.taker_fee_percentage.amount))
-    # Loop over tier fields:
-    for tier_field in [params.tiers.fields.fee_share_percentage,
-                       params.tiers.fields.tier_activation_fee,
-                       params.tiers.fields.withdrawal_fee]:
-        # Get constant name base for given field.
-        constant_name_base = tier_field.constant_name_base
-        # If on fee share percentage field, flag to correct to divisor.
-        correct_to_divisor = constant_name_base is \
-                params.tiers.fields.fee_share_percentage.constant_name_base
-        # Loop over tiers:
-        for tier_number, tier_fields in enumerate(params.tiers.amounts):
-            # Get amount for field at given tier.
-            amount = tier_fields[tier_field.field_index]
-            if correct_to_divisor: # Correct to divisor as needed:
-                amount = percent_to_divisor(amount)
-            else: # Otherwise correct to utility coin subunits:
-                amount = amount / usd_per_subunit
-            # Append to constants block the corresponding tier field.
-            block = block + format_constant_definition(
-                constant_name_base + seps.us + str(tier_number), amount)
-    return justify_constants(block)
-
-if __name__ == '__main__':
-    print(generate_constants_block(dec('4.00'), 8))
+    usd_per_subunit = utility_coin_usd_value / Decimal(1).shift(
+        utility_coin_decimals
+    )
+    # Index tier parameters.
+    divisors, activation_fees, withdrawal_fees = (
+        [percent_to_divisor(tier[0]) for tier in tiers],
+        [tier[1] for tier in tiers],
+        [tier[2] for tier in tiers],
+    )
+    return justify_constants(  # Return justified constants block.
+        "\n".join(
+            [
+                format_constant_definition(
+                    "MARKET_REGISTRATION_FEE",
+                    market_registration_fee / usd_per_subunit,
+                    is_genesis_block,
+                ),
+                format_constant_definition(
+                    "UNDERWRITER_REGISTRATION_FEE",
+                    underwriter_registration_fee / usd_per_subunit,
+                    is_genesis_block,
+                ),
+                format_constant_definition(
+                    "CUSTODIAN_REGISTRATION_FEE",
+                    custodian_registration_fee / usd_per_subunit,
+                    is_genesis_block,
+                ),
+                format_constant_definition(
+                    "TAKER_FEE_DIVISOR",
+                    percent_to_divisor(taker_fee_percentage),
+                    is_genesis_block,
+                ),
+                "\n".join(
+                    [
+                        format_constant_definition(
+                            f"FEE_SHARE_DIVISOR_{i}",
+                            divisor,
+                            is_genesis_block,
+                        )
+                        for i, divisor in enumerate(divisors)
+                    ],
+                ),
+                "\n".join(
+                    [
+                        format_constant_definition(
+                            f"TIER_ACTIVATION_FEE_{i}",
+                            activation_fee / usd_per_subunit,
+                            is_genesis_block,
+                        )
+                        for i, activation_fee in enumerate(activation_fees)
+                    ],
+                ),
+                "\n".join(
+                    [
+                        format_constant_definition(
+                            f"WITHDRAWAL_FEE_{i}",
+                            withdrawal_fee / usd_per_subunit,
+                            is_genesis_block,
+                        )
+                        for i, withdrawal_fee in enumerate(withdrawal_fees)
+                    ],
+                ),
+            ]
+        )
+    )

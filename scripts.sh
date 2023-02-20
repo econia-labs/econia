21c92695c5bfc35c1cf69d810aaaeaadefa8544a
alnoki <43892045+alnoki@users.noreply.github.com>
2023-02-19 19:53:59 -0700

    Consolidate scripting commands

--- scripts.sh
+++ scripts.sh
@@ -6,10 +6,13 @@ brew_url="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
 # Python package directory.
 python_dir="src/python/"
 
+# Path to this directory from Python directory.
+python_dir_inverse="../../"
+
 # Move package directory.
 move_dir="src/move/econia/"
 
-# Incentives modules.
+# Incentives Move module.
 incentives_module=$move_dir"sources/incentives.move"
 
 # Helper functions >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -33,6 +36,7 @@ case "$1" in
 
     # Initialize dev environment.
     init)
+        echo "Initializing developer environment"  # Print notice.
         if which brew &>/dev/null; then            # If brew installed:
             echo "Brew already installed"          # Print notice as such.
         else                                       # Otherwise:
@@ -42,10 +46,27 @@ case "$1" in
         brew_install aptos               # Install aptos CLI.
         brew_install poetry              # Insall poetry.
         brew_install shfmt               # Install shell script formatter.
-        cd src/python                    # Navigate to Python package directory.
+        cd $python_dir                   # Navigate to Python package directory.
         echo "Installing Python package" # Print notice.
         poetry install                   # Install the Python package and dependencies.
-        cd ../..                         # Navigate back to Econia root directory.
+        cd $python_dir_inverse           # Navigate back to Econia root directory.
+        ;;
+
+    # Format Python code
+    fp)
+        echo "Formatting Python code"
+        cd $python_dir # Change working directory to Python package.
+        # Find all files ending in .py, pass to autoflake command.
+        find . -name "*.py" | xargs \
+            poetry run autoflake \
+            --in-place \
+            --recursive \
+            --remove-all-unused-imports \
+            --remove-unused-variables \
+            --ignore-init-module-imports
+        poetry run isort .                  # Sort imports.
+        poetry run black . --line-length 80 # Format code.
+        cd $python_dir_inverse              # Navigate back to Econia root directory.
         ;;
 
     # Format shell scripts
@@ -57,17 +78,21 @@ case "$1" in
 
     # Set genesis parameters.
     sg)
-        echo "Setting genesis parameters." # Print notice.
-        cd python_dir # Change working directory to Python package.
+        echo "Setting genesis parameters" # Print notice.
+        cd $python_dir                    # Change working directory to Python package.
         # Run incentives CLI genesis command, passing arguments 2 onward.
         poetry run python -m econia.incentives genesis \
-            "../../"$incentives_module "${@:2}"
-        cd ../../ # Navigate back to Econia root directory.
+            $python_dir_inverse$incentives_module "${@:2}"
+        cd $python_dir_inverse # Navigate back to Econia root directory.
         ;;
 
-    # Develop Python package.
-    dp)
-        cd $python_dir # Change working directory to Python package.
+    # Test Python code.
+    tp)
+        echo "Running Python tests" # Print notice.
+        cd $python_dir              # Change working directory to Python package.
+        # Doctest all source.
+        find . -name "*.py" | xargs python -m doctest
+        cd $python_dir_inverse # Navigate back to Econia root directory.
         ;;
 
 esac

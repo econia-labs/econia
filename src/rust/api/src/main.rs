341f20be10438cd1d2c2876e712d65d75ff6082b
bogberry <bogberry@users.noreply.github.com>
2023-03-08 15:02:53 +0900

    axum server starter

--- src/rust/api/src/main.rs
+++ src/rust/api/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "api=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/", get(index))
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+
+    tracing::info!("Listening on http://{}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await
+        .unwrap();
+}
+
+async fn index() -> String {
+    String::from("Econia backend API")
 }

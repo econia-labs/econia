f3627b12d43bb12c2c9004d2859170fb25abcce1
bogberry <bogberry@users.noreply.github.com>
2023-03-14 19:00:10 +0900

    integration tests

---
+++ src/rust/db/tests/coin_tests.rs
@@ -0,0 +1,34 @@
+use db::{
+    create_coin,
+    establish_connection,
+    load_config,
+    models::Coin,
+    // schema::coins::dsl::*,
+};
+use diesel::prelude::*;
+
+#[test]
+fn test_create_coin() {
+    let config = load_config();
+    let conn = &mut establish_connection(config.database_url);
+
+    diesel::delete(db::schema::coins::table)
+        .execute(conn)
+        .expect("Error deleting coins");
+
+    create_coin(
+        conn,
+        "0x1",
+        "aptos_coin",
+        "AptosCoin",
+        Some("APT"),
+        Some("Aptos Coin"),
+        Some(8),
+    );
+
+    let db_coins = db::schema::coins::dsl::coins
+        .load::<Coin>(conn)
+        .expect("Could not query coins.");
+
+    assert_eq!(db_coins.len(), 1);
+}

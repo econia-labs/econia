2ea5170bcdfe68fb150fdc2ca4d364b39ace6309
bogberry <bogberry@users.noreply.github.com>
2023-03-14 18:58:46 +0900

    update market schema

--- src/rust/db/migrations/2023-03-08-062527_create_coins/down.sql
+++ src/rust/db/migrations/2023-03-08-062527_create_coins/down.sql
@@ -1,3 +1,7 @@
 -- This file should undo anything in `up.sql`
-drop table orderbooks;
+drop table recognized_market_events;
+drop type market_event_type;
+drop table recognized_markets;
+drop table market_registration_events;
+drop table markets;
 drop table coins;
--- src/rust/db/migrations/2023-03-08-062527_create_coins/up.sql
+++ src/rust/db/migrations/2023-03-08-062527_create_coins/up.sql
@@ -1,16 +1,78 @@
+-- Corresponds to aptos_std::type_info::TypeInfo and aptos_framework::coin::CoinInfo.
 create table coins (
-    symbol varchar(8) not null primary key,
-    name text not null,
-    decimals smallint not null,
-    address text not null
+    id serial not null primary key,
+    account_address varchar(70) not null,
+    module_name text not null,
+    struct_name text not null,
+    symbol varchar(8),
+    name text,
+    decimals smallint
 );
 
-create table orderbooks (
-    id int not null primary key,
-    base varchar(8) not null references coins(symbol),
-    quote varchar(8) not null references coins(symbol),
-    lot_size int not null,
-    tick_size int not null,
-    min_size int not null,
-    underwriter_id int not null
+-- Corresponds to econia::registry::MarketInfo.
+-- Only recognized markets should be stored in the api database.
+create table markets (
+    market_id numeric(20) not null primary key,
+    base_id serial not null,
+    base_name_generic text,
+    quote_id serial not null,
+    lot_size numeric(20) not null,
+    tick_size numeric(20) not null,
+    min_size numeric(20) not null,
+    underwriter_id numeric(20) not null,
+    created_at timestamp not null,
+    foreign key (base_id) references coins(id),
+    foreign key (quote_id) references coins(id)
+);
+
+-- Corresponds to econia::registry::MarketRegistrationEvent
+create table market_registration_events (
+    market_id numeric(20) not null primary key,
+    time timestamp not null,
+    base_id serial not null,
+    base_name_generic text,
+    quote_id serial not null,
+    lot_size numeric(20) not null,
+    tick_size numeric(20) not null,
+    min_size numeric(20) not null
+);
+
+create function create_market() returns trigger as $create_market$ begin
+insert into markets
+values (
+        NEW.market,
+        NEW.base_id,
+        NEW.base_name_generic,
+        NEW.quote_id,
+        NEW.lot_size,
+        NEW.tick_size,
+        NEW.min_size,
+        NEW.underwriter_id,
+        NEW.time
+    );
+
+return NEW;
+
+end;
+
+$create_market$ language plpgsql;
+
+-- Corresponds to econia::registry::RecognizedMarketInfo
+-- This id does not need to exist, but diesel only supports tables with primary keys
+create table recognized_markets (
+    id serial not null primary key,
+    market_id numeric(20) not null,
+    foreign key (market_id) references markets(market_id)
+);
+
+-- Type of events that can be emitted for a recognized market.
+create type market_event_type as enum ('add', 'remove', 'update');
+
+-- Corresponds to econia::registry::RecognizedMarketEvent. 
+create table recognized_market_events (
+    market_id numeric(20) not null primary key,
+    event_type market_event_type not null,
+    lot_size numeric(20),
+    tick_size numeric(20),
+    min_size numeric(20)
 );

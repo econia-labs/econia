fc6d9d3ec4e060a7a18de7bf0223a021e5a5730a
bogberry <bogberry@users.noreply.github.com>
2023-03-16 11:14:11 +0900

    add unit test for generic market

--- src/rust/db/tests/market_tests.rs
+++ src/rust/db/tests/market_tests.rs
@@ -6,12 +6,7 @@ use db::{
 };
 use diesel::prelude::*;
 
-#[test]
-fn test_register_market() {
-    let config = load_config();
-    let conn = &mut establish_connection(config.database_url);
-
-    // Delete all entries in the tables used before running tests.
+fn reset_market_databases(conn: &mut PgConnection) {
     diesel::delete(db::schema::markets::table)
         .execute(conn)
         .expect("Error deleting markets table");
@@ -23,6 +18,15 @@ fn test_register_market() {
     diesel::delete(db::schema::coins::table)
         .execute(conn)
         .expect("Error deleting coins table");
+}
+
+#[test]
+fn test_register_coin_market() {
+    let config = load_config();
+    let conn = &mut establish_connection(config.database_url);
+
+    // Delete all entries in the tables used before running tests.
+    reset_market_databases(conn);
 
     // Register coins first, so we can satisfy the foreign key constraint in markets.
     let aptos_coin = create_coin(
@@ -55,6 +59,63 @@ fn test_register_market() {
         &aptos_coin.module_name,
         &aptos_coin.struct_name,
         None,
+        &tusdc_coin.account_address,
+        &tusdc_coin.module_name,
+        &tusdc_coin.struct_name,
+        1000.into(),
+        1000.into(),
+        1000.into(),
+        0.into(),
+    );
+
+    // Check that the market registration events table has one entry.
+    let db_market_registration_events =
+        db::schema::market_registration_events::dsl::market_registration_events
+            .load::<MarketRegistrationEvent>(conn)
+            .expect("Could not query market registration events.");
+
+    assert_eq!(db_market_registration_events.len(), 1);
+
+    // Check that the markets table has one entry.
+    let db_markets = db::schema::markets::dsl::markets
+        .load::<Market>(conn)
+        .expect("Could not query markets.");
+
+    assert_eq!(db_markets.len(), 1);
+}
+
+#[test]
+fn test_register_generic_market() {
+    let config = load_config();
+    let conn = &mut establish_connection(config.database_url);
+
+    // Delete all entries in the tables used before running tests.
+    reset_market_databases(conn);
+
+    // Register GenericAsset to coins table to satisfy foreign key constraint.
+    create_coin(conn, "econia", "registry", "GenericAsset", None, None, None);
+
+    // Register quote coin.
+    let tusdc_coin = create_coin(
+        conn,
+        "0x7c36a610d1cde8853a692c057e7bd2479ba9d5eeaeceafa24f125c23d2abf942",
+        "test_usdc",
+        "TestUSDCoin",
+        Some("tUSDC"),
+        Some("Test USDC"),
+        Some(6),
+    );
+
+    // Register the market. Adding a new market registration event should create a new entry
+    // in the markets table as well.
+    register_market(
+        conn,
+        1.into(),
+        Utc::now(),
+        "econia",
+        "registry",
+        "GenericAsset",
+        Some("APT-PERP"),
         &tusdc_coin.account_address,
         &tusdc_coin.module_name,
         &tusdc_coin.struct_name,

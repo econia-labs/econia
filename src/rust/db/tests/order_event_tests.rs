d72c9ddb82125addfe6fe7c84081e0fd8fe6a74c
bogberry <bogberry@users.noreply.github.com>
2023-03-16 18:23:26 +0900

    add test for order change to remaining size 0

--- src/rust/db/tests/order_event_tests.rs
+++ src/rust/db/tests/order_event_tests.rs
@@ -182,3 +182,66 @@ fn test_change_order() {
     // Clean up tables.
     reset_order_tables(conn);
 }
+
+#[test]
+fn test_change_order_to_remaining_size_zero() {
+    let config = load_config();
+    let conn = &mut establish_connection(config.database_url);
+
+    // Delete all entries in the tables used before running tests.
+    reset_order_tables(conn);
+
+    // Set up market
+    let market = setup_market(conn);
+
+    add_maker_event(
+        conn,
+        market.market_id.clone(),
+        Side::Buy,
+        123.into(),
+        "0x123",
+        None,
+        MakerEventType::Place,
+        1000.into(),
+        1000.into(),
+        Utc::now(),
+    );
+
+    add_maker_event(
+        conn,
+        market.market_id,
+        Side::Buy,
+        123.into(),
+        "0x123",
+        None,
+        MakerEventType::Change,
+        0.into(),
+        1000.into(),
+        Utc::now(),
+    );
+
+    // Check that the maker events table has one entry.
+    let db_maker_events = db::schema::maker_events::dsl::maker_events
+        .load::<MakerEvent>(conn)
+        .expect("Could not query maker events.");
+
+    assert_eq!(db_maker_events.len(), 2);
+
+    // Check that the orders table has one entry.
+    let db_orders = db::schema::orders::dsl::orders
+        .load::<Order>(conn)
+        .expect("Could not query orders.");
+
+    assert_eq!(db_orders.len(), 1);
+
+    let db_order = db_orders.get(0).unwrap();
+    assert_eq!(db_order.size, BigDecimal::from_i32(0).unwrap());
+    assert_eq!(db_order.price, BigDecimal::from_i32(1000).unwrap());
+    assert_eq!(db_order.remaining_size, BigDecimal::from_i32(0).unwrap());
+    assert_eq!(db_order.order_state, OrderState::Filled);
+
+    println!("{:#?}", db_order);
+
+    // Clean up tables.
+    reset_order_tables(conn);
+}

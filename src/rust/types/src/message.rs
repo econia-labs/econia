76b35b5550b538f263fbf7dab18a46e4d1a52ad0
bogberry <bogberry@users.noreply.github.com>
2023-03-27 22:26:44 +0900

    make json format more consistent

--- src/rust/types/src/message.rs
+++ src/rust/types/src/message.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::order::Order;
 
 #[derive(Debug, Deserialize, Serialize, Clone, Hash, PartialEq, Eq)]
-#[serde(tag = "channel", rename_all = "snake_case")]
+#[serde(tag = "channel", content = "params", rename_all = "snake_case")]
 pub enum Channel {
     Orders {
         market_id: u64,
@@ -19,13 +19,13 @@ pub enum Channel {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(tag = "channel", rename_all = "snake_case")]
+#[serde(tag = "channel", content = "data", rename_all = "snake_case")]
 pub enum Update {
-    Orders { data: Order },
+    Orders(Order),
     // TODO add more update types
 }
 
-#[derive(Debug, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
 #[serde(tag = "method", rename_all = "snake_case")]
 pub enum InboundMessage {
     Ping,
@@ -34,12 +34,25 @@ pub enum InboundMessage {
 }
 
 #[derive(Debug, Serialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum ConfirmMethod {
+    Subscribe,
+    Unsubscribe,
+}
+
+#[derive(Debug, Serialize, Clone)]
 #[serde(tag = "event", rename_all = "snake_case")]
 pub enum OutboundMessage {
     Pong,
-    Confirm(Channel),
+    Confirm {
+        #[serde(flatten)]
+        channel: Channel,
+        method: ConfirmMethod,
+    },
     Update(Update),
-    Error { message: String },
+    Error {
+        message: String,
+    },
     Close,
 }
 
@@ -58,7 +71,7 @@ mod tests {
 
     #[test]
     fn test_deserialize_subscribe_message() {
-        let s = r#"{"method":"subscribe","channel":"orders","market_id":0,"user_address":"0x1"}"#;
+        let s = r#"{"method":"subscribe","channel":"orders","params":{"market_id":0,"user_address":"0x1"}}"#;
         let msg: InboundMessage = serde_json::from_str(s).unwrap();
         assert_eq!(
             msg,
@@ -77,15 +90,34 @@ mod tests {
     }
 
     #[test]
-    fn test_serialize_confirm_message() {
-        let msg = OutboundMessage::Confirm(Channel::Orders {
-            market_id: 0,
-            user_address: "0x1".into(),
-        });
+    fn test_serialize_confirm_subscribe_message() {
+        let msg = OutboundMessage::Confirm {
+            channel: Channel::Orders {
+                market_id: 0,
+                user_address: "0x1".into(),
+            },
+            method: ConfirmMethod::Subscribe,
+        };
         let s = serde_json::to_string(&msg).unwrap();
         assert_eq!(
             s,
-            r#"{"event":"confirm","channel":"orders","market_id":0,"user_address":"0x1"}"#
+            r#"{"event":"confirm","channel":"orders","params":{"market_id":0,"user_address":"0x1"},"method":"subscribe"}"#
+        );
+    }
+
+    #[test]
+    fn test_serialize_confirm_unsubscribe_message() {
+        let msg = OutboundMessage::Confirm {
+            channel: Channel::Orders {
+                market_id: 0,
+                user_address: "0x1".into(),
+            },
+            method: ConfirmMethod::Unsubscribe,
+        };
+        let s = serde_json::to_string(&msg).unwrap();
+        assert_eq!(
+            s,
+            r#"{"event":"confirm","channel":"orders","params":{"market_id":0,"user_address":"0x1"},"method":"unsubscribe"}"#
         );
     }
 }

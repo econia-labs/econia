6f052e3d9aa11344ac94755753fd3ed4521419c6
bogberry <bogberry@users.noreply.github.com>
2023-03-27 22:27:24 +0900

    separate function for getting outbound message enum

--- src/rust/api/src/ws.rs
+++ src/rust/api/src/ws.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::{
     collections::HashSet,
     net::SocketAddr,
@@ -17,7 +18,7 @@ use futures_util::{
 };
 use regex::Regex;
 use tokio::sync::{broadcast, mpsc};
-use types::message::{Channel, InboundMessage, OutboundMessage, Update};
+use types::message::{Channel, ConfirmMethod, InboundMessage, OutboundMessage, Update};
 
 use crate::{error::WebSocketError, AppState};
 
@@ -33,9 +34,11 @@ pub async fn ws_handler(
 async fn forward_message_handler(
     mut sender: SplitSink<WebSocket, Message>,
     mut rx: mpsc::Receiver<OutboundMessage>,
+    who: SocketAddr,
 ) -> Result<(), WebSocketError> {
     while let Some(msg) = rx.recv().await {
         let s = serde_json::to_string(&msg)?;
+        tracing::info!("sending message `{}` to client {}", s, who);
         sender.send(Message::Text(s)).await?;
     }
     Ok(())
@@ -48,10 +51,10 @@ async fn outbound_message_handler(
 ) -> Result<(), WebSocketError> {
     while let Ok(update) = brx.recv().await {
         let subbed = match update {
-            Update::Orders { ref data } => {
+            Update::Orders(ref order) => {
                 let ch = Channel::Orders {
-                    market_id: data.market_id,
-                    user_address: data.user_address.clone(),
+                    market_id: order.market_id,
+                    user_address: order.user_address.clone(),
                 };
                 let lock = subs.lock().unwrap();
                 (*lock).contains(&ch)
@@ -65,61 +68,95 @@ async fn outbound_message_handler(
     Ok(())
 }
 
+fn get_response_message(
+    msg_i: InboundMessage,
+    subs: &Arc<Mutex<HashSet<Channel>>>,
+) -> Result<OutboundMessage, WebSocketError> {
+    match msg_i {
+        InboundMessage::Ping => Ok(OutboundMessage::Pong),
+        InboundMessage::Subscribe(channel) => {
+            let subbed = {
+                let mut lock = subs.lock().unwrap();
+                let b = (*lock).contains(&channel);
+                if !b {
+                    (*lock).insert(channel.clone());
+                }
+                b
+            };
+            if subbed {
+                let s = serde_json::to_string(&channel)?;
+                Ok(OutboundMessage::Error {
+                    message: format!("already subscribed to channel `{}`", s),
+                })
+            } else {
+                Ok(OutboundMessage::Confirm {
+                    channel,
+                    method: ConfirmMethod::Subscribe,
+                })
+            }
+        }
+        InboundMessage::Unsubscribe(channel) => {
+            let subbed = {
+                let mut lock = subs.lock().unwrap();
+                let b = (*lock).contains(&channel);
+                if b {
+                    (*lock).remove(&channel);
+                }
+                b
+            };
+            if !subbed {
+                let s = serde_json::to_string(&channel)?;
+                Ok(OutboundMessage::Error {
+                    message: format!("not subscribed to channel `{}`", s),
+                })
+            } else {
+                Ok(OutboundMessage::Confirm {
+                    channel,
+                    method: ConfirmMethod::Unsubscribe,
+                })
+            }
+        }
+    }
+}
+
 async fn inbound_message_handler(
     mut receiver: SplitStream<WebSocket>,
     tx: mpsc::Sender<OutboundMessage>,
     subs: &Arc<Mutex<HashSet<Channel>>>,
     who: SocketAddr,
 ) -> Result<(), WebSocketError> {
+    // define regex to remove line breaks and whitespace
+    let re = Regex::new(r"\n|\s+").unwrap();
     while let Some(Ok(msg)) = receiver.next().await {
         match msg {
             Message::Text(s) => {
-                if let Ok(msg_i) = serde_json::from_str::<InboundMessage>(&s) {
-                    match msg_i {
-                        InboundMessage::Ping => {
-                            tracing::info!("received ping message from client {}", who);
-                            let msg_o = OutboundMessage::Pong;
-                            tx.send(msg_o).await?;
-                        }
-                        InboundMessage::Subscribe(channel) => {
-                            tracing::info!("received subscribe message from client {}", who);
-                            let already_subbed = {
-                                let lock = subs.lock().unwrap();
-                                (*lock).contains(&channel)
-                            };
-
-                            if already_subbed {
-                                let channel_s = serde_json::to_string(&channel)?;
-                                tx.send(OutboundMessage::Error {
-                                    message: format!(
-                                        "already subscribed to channel `{}`",
-                                        channel_s
-                                    ),
-                                })
-                                .await?;
-                            } else {
-                                {
-                                    let mut lock = subs.lock().unwrap();
-                                    (*lock).insert(channel.clone());
-                                }
-                                tx.send(OutboundMessage::Confirm(channel)).await?;
-                            }
-                        }
-                        _ => {
-                            // TODO
-                        }
+                tracing::info!(
+                    "received message from client {}: {}",
+                    who,
+                    re.replace_all(&s, ""),
+                );
+                match serde_json::from_str::<InboundMessage>(&s) {
+                    Ok(msg_i) => {
+                        let msg_o = get_response_message(msg_i, subs)?;
+                        tx.send(msg_o).await?;
                     }
-                } else {
-                    let re = Regex::new(r"\n|\s+").unwrap();
-                    tracing::warn!(
-                        "could not parse message `{}` received from client {}",
-                        re.replace_all(&s, ""),
-                        who
-                    );
-                    let msg_o = OutboundMessage::Error {
-                        message: "could not parse message".into(),
-                    };
-                    tx.send(msg_o).await?;
+                    Err(e) => {
+                        tracing::warn!(
+                            "could not parse message `{}` received from client {}: {}",
+                            re.replace_all(&s, ""),
+                            who,
+                            e
+                        );
+                        // remove `at position` text from serde error messages,
+                        // since it is not useful information for websocket clients.
+                        let e_msg = if let Some(m) = e.to_string().split(" at ").next() {
+                            m.to_string()
+                        } else {
+                            "could not parse message".to_string()
+                        };
+                        let msg_o = OutboundMessage::Error { message: e_msg };
+                        tx.send(msg_o).await?;
+                    }
                 }
             }
             Message::Close(c) => {
@@ -145,7 +182,7 @@ async fn handle_socket(ws: WebSocket, btx: broadcast::Sender<Update>, who: Socke
     let subs = Arc::new(Mutex::new(HashSet::<Channel>::new()));
 
     let mut fwd_task = tokio::spawn(async move {
-        if let Err(e) = forward_message_handler(sender, mrx).await {
+        if let Err(e) = forward_message_handler(sender, mrx, who).await {
             tracing::error!(
                 "websocket connection with client {} failed on message forward task: {}",
                 who,

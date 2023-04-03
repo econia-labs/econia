3120b91a9fb5f5352455086ac2705d637d392890
bogberry <bogberry@users.noreply.github.com>
2023-04-03 21:56:48 +0900

    add invalid time range error type

--- src/rust/api/src/error.rs
+++ src/rust/api/src/error.rs
@@ -7,6 +7,9 @@ pub enum ApiError {
     #[error("404 Not Found")]
     NotFound,
 
+    #[error("Invalid time range")]
+    InvalidTimeRange,
+
     #[error(transparent)]
     SqlxError(#[from] sqlx::error::Error),
 
@@ -22,6 +25,7 @@ impl IntoResponse for ApiError {
         tracing::error!("{}", self.to_string());
         let res = match self {
             Self::NotFound => (StatusCode::NOT_FOUND, self.to_string()),
+            Self::InvalidTimeRange => (StatusCode::BAD_REQUEST, self.to_string()),
             Self::SqlxError(_) => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()),
             Self::TypeError(_) => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()),
             Self::ParseBigDecimal(_) => (StatusCode::BAD_REQUEST, self.to_string()),

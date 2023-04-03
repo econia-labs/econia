4174af7d5ea7dcc721390ff1aa0195b7e5fb1acb
bogberry <bogberry@users.noreply.github.com>
2023-04-03 18:52:02 +0900

    add interval enum

--- src/rust/types/src/bar.rs
+++ src/rust/types/src/bar.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
@@ -10,4 +12,22 @@ pub struct Bar {
     pub low: u64,
     pub close: u64,
     pub volume: u64,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum Interval {
+    #[serde(rename = "1m")]
+    I1m,
+    #[serde(rename = "5m")]
+    I5m,
+}
+
+impl fmt::Display for Interval {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            Self::I1m => "1m",
+            Self::I5m => "5m",
+        };
+        write!(f, "{}", s)
+    }
 }

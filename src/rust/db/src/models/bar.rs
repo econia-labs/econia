62c2b795da49b3d441b870076689467f2ee00d76
bogberry <bogberry@users.noreply.github.com>
2023-04-03 11:40:50 +0900

    add bar function

--- src/rust/db/src/models/bar.rs
+++ src/rust/db/src/models/bar.rs
@@ -4,9 +4,20 @@ use diesel::prelude::*;
 
 use crate::schema::bars_1m;
 
-#[derive(Clone, Debug, Queryable, Insertable)]
-#[diesel(table_name = bars_1m)]
+#[derive(Clone, Debug, Queryable)]
 pub struct Bar {
+    pub market_id: BigDecimal,
+    pub start_time: DateTime<Utc>,
+    pub open: BigDecimal,
+    pub high: BigDecimal,
+    pub low: BigDecimal,
+    pub close: BigDecimal,
+    pub volume: BigDecimal,
+}
+
+#[derive(Debug, Insertable)]
+#[diesel(table_name = bars_1m)]
+pub struct NewBar {
     pub market_id: BigDecimal,
     pub start_time: DateTime<Utc>,
     pub open: BigDecimal,

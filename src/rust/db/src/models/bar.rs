ce306212a0f955385114041190f35f85c046ef95
bogberry <bogberry@users.noreply.github.com>
2023-04-03 12:43:11 +0900

    add convert function

--- src/rust/db/src/models/bar.rs
+++ src/rust/db/src/models/bar.rs
@@ -1,6 +1,7 @@
-use bigdecimal::BigDecimal;
+use bigdecimal::{BigDecimal, ToPrimitive};
 use chrono::{DateTime, Utc};
 use diesel::prelude::*;
+use types::error::TypeError;
 
 use crate::schema::bars_1m;
 
@@ -25,4 +26,40 @@ pub struct NewBar {
     pub low: BigDecimal,
     pub close: BigDecimal,
     pub volume: BigDecimal,
+}
+
+impl TryFrom<Bar> for types::bar::Bar {
+    type Error = TypeError;
+
+    fn try_from(value: Bar) -> Result<Self, Self::Error> {
+        let market_id = value.market_id.to_u64().ok_or(TypeError::ConversionError {
+            name: "market_id".into(),
+        })?;
+        let open = value.open.to_u64().ok_or(TypeError::ConversionError {
+            name: "open".into(),
+        })?;
+        let high = value.high.to_u64().ok_or(TypeError::ConversionError {
+            name: "high".into(),
+        })?;
+        let low = value
+            .low
+            .to_u64()
+            .ok_or(TypeError::ConversionError { name: "low".into() })?;
+        let close = value.close.to_u64().ok_or(TypeError::ConversionError {
+            name: "close".into(),
+        })?;
+        let volume = value.volume.to_u64().ok_or(TypeError::ConversionError {
+            name: "volume".into(),
+        })?;
+
+        Ok(types::bar::Bar {
+            market_id,
+            start_time: value.start_time,
+            open,
+            high,
+            low,
+            close,
+            volume,
+        })
+    }
 }

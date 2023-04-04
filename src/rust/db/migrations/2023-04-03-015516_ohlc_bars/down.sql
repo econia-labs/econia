0c4ad32c25a6d4462afc250db1f7811861ccb4fc
bogberry <bogberry@users.noreply.github.com>
2023-04-04 17:34:06 +0900

    add trigger for 15 minute interval

--- src/rust/db/migrations/2023-04-03-015516_ohlc_bars/down.sql
+++ src/rust/db/migrations/2023-04-03-015516_ohlc_bars/down.sql
@@ -1,6 +1,12 @@
+drop trigger handle_5m_interval_end_trigger on bars_5m;
+
+drop function handle_5m_interval_end;
+
 drop trigger handle_1m_interval_end_trigger on bars_1m;
 
 drop function handle_1m_interval_end;
+
+drop table bars_15m;
 
 drop table bars_5m;
 

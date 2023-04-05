8d83db54c837aeafc0ec47b93ab41a2736847cb3
bogberry <bogberry@users.noreply.github.com>
2023-04-05 13:34:56 +0900

    trigger for 30 minute migration

--- src/rust/db/migrations/2023-04-03-015516_ohlc_bars/down.sql
+++ src/rust/db/migrations/2023-04-03-015516_ohlc_bars/down.sql
@@ -1,3 +1,7 @@
+drop trigger handle_15m_interval_end_trigger on bars_15m;
+
+drop function handle_15m_interval_end;
+
 drop trigger handle_5m_interval_end_trigger on bars_5m;
 
 drop function handle_5m_interval_end;
@@ -5,6 +9,8 @@ drop function handle_5m_interval_end;
 drop trigger handle_1m_interval_end_trigger on bars_1m;
 
 drop function handle_1m_interval_end;
+
+drop table bars_30m;
 
 drop table bars_15m;
 

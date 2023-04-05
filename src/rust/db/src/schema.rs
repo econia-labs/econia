322007013f3b09c0705658d632689089ec616118
bogberry <bogberry@users.noreply.github.com>
2023-04-05 13:35:11 +0900

    update schema again

--- src/rust/db/src/schema.rs
+++ src/rust/db/src/schema.rs
@@ -43,6 +43,18 @@ diesel::table! {
 }
 
 diesel::table! {
+    bars_30m (market_id, start_time) {
+        market_id -> Numeric,
+        start_time -> Timestamptz,
+        open -> Numeric,
+        high -> Numeric,
+        low -> Numeric,
+        close -> Numeric,
+        volume -> Numeric,
+    }
+}
+
+diesel::table! {
     bars_5m (market_id, start_time) {
         market_id -> Numeric,
         start_time -> Timestamptz,
@@ -192,6 +204,7 @@ diesel::table! {
 
 diesel::joinable!(bars_15m -> markets (market_id));
 diesel::joinable!(bars_1m -> markets (market_id));
+diesel::joinable!(bars_30m -> markets (market_id));
 diesel::joinable!(bars_5m -> markets (market_id));
 diesel::joinable!(fills -> markets (market_id));
 diesel::joinable!(maker_events -> markets (market_id));
@@ -203,6 +216,7 @@ diesel::joinable!(taker_events -> markets (market_id));
 diesel::allow_tables_to_appear_in_same_query!(
     bars_15m,
     bars_1m,
+    bars_30m,
     bars_5m,
     coins,
     fills,

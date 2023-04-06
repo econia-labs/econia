877ad2d2f51198bb1035d45c3b5885df97c54330
bogberry <bogberry@users.noreply.github.com>
2023-04-06 16:10:27 +0900

    add another unit test

--- src/rust/db/tests/bar_tests.rs
+++ src/rust/db/tests/bar_tests.rs
@@ -131,3 +131,56 @@ fn test_add_15m_bars() {
     // Clean up tables.
     reset_tables(conn);
 }
+
+#[test]
+fn test_add_30m_bars() {
+    let config = load_config();
+    let conn = &mut establish_connection(config.database_url);
+
+    // Delete all entries in the tables used before running tests.
+    reset_tables(conn);
+
+    // Set up market.
+    let market = setup_market(conn);
+
+    for i in 0..90 {
+        let start_time = Utc.with_ymd_and_hms(2023, 4, 5, i / 60, i % 60, 0).unwrap();
+        let bar = NewBar {
+            market_id: market.market_id.clone(),
+            start_time,
+            open: BigDecimal::from((i + 2) * 100),
+            high: BigDecimal::from((i + 4) * 100),
+            low: BigDecimal::from((i + 1) * 100),
+            close: BigDecimal::from((i + 3) * 100),
+            volume: BigDecimal::from(100),
+        };
+        add_bar(conn, &bar);
+    }
+
+    let db_1m_bars = db::schema::bars_1m::dsl::bars_1m
+        .load::<Bar>(conn)
+        .expect("Could not query 1m bars.");
+
+    assert_eq!(db_1m_bars.len(), 90);
+
+    let db_5m_bars = db::schema::bars_5m::dsl::bars_5m
+        .load::<Bar>(conn)
+        .expect("Could not query 5m bars.");
+
+    assert_eq!(db_5m_bars.len(), 18);
+
+    let db_15m_bars = db::schema::bars_15m::dsl::bars_15m
+        .load::<Bar>(conn)
+        .expect("Could not query 15m bars.");
+
+    assert_eq!(db_15m_bars.len(), 6);
+
+    let db_15m_bars = db::schema::bars_30m::dsl::bars_30m
+        .load::<Bar>(conn)
+        .expect("Could not query 30m bars.");
+
+    assert_eq!(db_15m_bars.len(), 3);
+
+    // Clean up tables.
+    reset_tables(conn);
+}

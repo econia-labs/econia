6ba2c4be68db9b70398224636786cce018574a6c
qdrs <qdrs@proton.me>
2023-04-08 01:13:17 +0800

    add book types

--- src/rust/types/src/book.rs
+++ src/rust/types/src/book.rs
@@ -1,11 +1,20 @@
 use std::collections::{BTreeMap, HashMap};
 
+use serde::{Deserialize, Serialize};
+
 use crate::order::{Order, Side};
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct OrderBook {
     asks: BTreeMap<u64, Vec<Order>>,
     bids: BTreeMap<u64, Vec<Order>>,
     orders_to_price_level: HashMap<u64, (Side, u64)>,
+}
+
+impl Default for OrderBook {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl OrderBook {
@@ -31,38 +40,26 @@ impl OrderBook {
         }
     }
 
-    pub fn get_book_side_price_level(&self, order_id: u64) -> (Side, u64) {
-        let (side, price) = self
-            .orders_to_price_level
+    pub fn get_book_side_price_level(&self, order_id: u64) -> Option<(Side, u64)> {
+        self.orders_to_price_level
             .get(&order_id)
-            .expect("invalid state, order is missing");
-        (*side, *price)
+            .map(|(s, p)| (*s, *p))
     }
 
-    pub fn get_order(&self, order_id: u64) -> &Order {
-        let (side, price) = self.get_book_side_price_level(order_id);
-        let book_side = self.get_side(side);
-        let orders = book_side
-            .get(&price)
-            .expect("invalid state, price level is missing");
-        let order = orders
-            .iter()
-            .find(|o| o.market_order_id == order_id)
-            .expect("invalid state, order is missing");
-        order
+    pub fn get_order(&self, order_id: u64) -> Option<&Order> {
+        self.get_book_side_price_level(order_id).and_then(|(s, p)| {
+            self.get_side(s)
+                .get(&p)
+                .and_then(|o| o.iter().find(|o| o.market_order_id == order_id))
+        })
     }
 
-    pub fn get_order_mut(&mut self, order_id: u64) -> &mut Order {
-        let (side, price) = self.get_book_side_price_level(order_id);
-        let book_side = self.get_side_mut(side);
-        let orders = book_side
-            .get_mut(&price)
-            .expect("invalid state, price level is missing");
-        let order = orders
-            .iter_mut()
-            .find(|o| o.market_order_id == order_id)
-            .expect("invalid state, order is missing");
-        order
+    pub fn get_order_mut(&mut self, order_id: u64) -> Option<&mut Order> {
+        self.get_book_side_price_level(order_id).and_then(|(s, p)| {
+            self.get_side_mut(s)
+                .get_mut(&p)
+                .and_then(|o| o.iter_mut().find(|o| o.market_order_id == order_id))
+        })
     }
 
     pub fn add_order(&mut self, order: Order) {
@@ -80,9 +77,13 @@ impl OrderBook {
             .insert(market_order_id, (side, price));
     }
 
-    pub fn remove_order(&mut self, order_id: u64) -> Order {
-        self.orders_to_price_level.remove(&order_id);
-        let (side, price) = self.get_book_side_price_level(order_id);
+    // removes an order if it exists
+    pub fn remove_order(&mut self, order_id: u64) -> Option<Order> {
+        self.orders_to_price_level.remove(&order_id)?;
+        let (side, price) = self
+            .get_book_side_price_level(order_id)
+            .expect("invalid state");
+
         let book_side = self.get_side_mut(side);
         let level = book_side
             .get_mut(&price)
@@ -98,7 +99,13 @@ impl OrderBook {
             book_side.remove(&price);
         }
 
-        order
+        Some(order)
     }
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PriceLevel {
+    pub market_id: u64,
+    pub price: u64,
+    pub size: u64,
+}

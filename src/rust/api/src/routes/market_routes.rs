9966fcb5b338d5df7c1c15db3d24e461d706a053
bogberry <bogberry@users.noreply.github.com>
2023-04-11 15:09:00 +0900

    testing market history endpoint

--- src/rust/api/src/routes/market_routes.rs
+++ src/rust/api/src/routes/market_routes.rs
@@ -166,3 +166,69 @@ pub async fn get_fills(
 
     Ok(Json(fills))
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use chrono::TimeZone;
+    use sqlx::PgPool;
+    use tokio::sync::broadcast;
+    use tower::ServiceExt;
+
+    use super::*;
+    use crate::{get_market_ids, load_config, routes::router, start_redis_channels};
+
+    #[tokio::test]
+    async fn test_get_market_history() {
+        let market_id = "0";
+        let resolution = Resolution::I1m;
+
+        let from = Utc
+            .with_ymd_and_hms(2023, 4, 5, 0, 0, 0)
+            .unwrap()
+            .timestamp();
+        let to = Utc
+            .with_ymd_and_hms(2023, 4, 5, 0, 10, 0)
+            .unwrap()
+            .timestamp();
+
+        let config = load_config();
+
+        let pool = PgPool::connect(&config.database_url)
+            .await
+            .expect("Could not connect to DATABASE_URL");
+
+        let market_ids = get_market_ids(pool.clone()).await;
+        if market_ids.is_empty() {
+            tracing::warn!("no markets registered in database");
+        }
+
+        let (btx, _brx) = broadcast::channel(16);
+        let _conn = start_redis_channels(config.redis_url, market_ids, btx.clone()).await;
+
+        let state = AppState { pool, sender: btx };
+        let app = router(state);
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri(format!(
+                        "/market/{}/history?resolution={}&from={}&to={}",
+                        market_id, resolution, from, to
+                    ))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let res = serde_json::from_slice::<Vec<types::bar::Bar>>(&body);
+        assert!(res.is_ok());
+    }
+}

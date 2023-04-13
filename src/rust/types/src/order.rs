e7f23cf30a0ae1998276b45e795b8107a9837891
qdrs <71022090+qdrs@users.noreply.github.com>
2023-04-13 23:14:45 +0800

    ECO-86: Rust SDK Entry Functions (#138)
    * stub
    
    * some sdk work
    
    * update submodule
    
    * create transaction builder
    
    * added incentives entry functions
    
    * market entry functions
    
    * registry functions
    
    * user functions
    
    * enum types
    
    * remove getters and rename vars
    
    * remove panic
    
    * apostrophe
    
    * use thiserror instead of anyhow

--- src/rust/types/src/order.rs
+++ src/rust/types/src/order.rs
@@ -5,6 +5,7 @@ use crate::error::TypeError;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
+#[repr(u8)]
 pub enum Side {
     Ask,
     Bid,
@@ -24,10 +25,91 @@ impl TryFrom<u8> for Side {
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
-            0 => Ok(Side::Ask),
-            1 => Ok(Side::Bid),
+            0 => Ok(Self::Ask),
+            1 => Ok(Self::Bid),
             _ => Err(TypeError::ConversionError {
                 name: "Side".to_string(),
+            }),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+#[repr(u8)]
+pub enum AdvanceStyle {
+    Ticks,
+    Percent,
+}
+
+impl From<bool> for AdvanceStyle {
+    fn from(value: bool) -> Self {
+        match value {
+            false => Self::Ticks,
+            true => Self::Percent,
+        }
+    }
+}
+
+impl TryFrom<u8> for AdvanceStyle {
+    type Error = TypeError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::Ticks),
+            1 => Ok(Self::Percent),
+            _ => Err(TypeError::ConversionError {
+                name: "AdvanceStyle".to_string(),
+            }),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+#[repr(u8)]
+pub enum SelfMatchBehavior {
+    Abort,
+    CancelBoth,
+    CancelMaker,
+    CancelTaker,
+}
+
+impl TryFrom<u8> for SelfMatchBehavior {
+    type Error = TypeError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::Abort),
+            1 => Ok(Self::CancelBoth),
+            2 => Ok(Self::CancelMaker),
+            3 => Ok(Self::CancelTaker),
+            _ => Err(TypeError::ConversionError {
+                name: "SelfMatchBehavior".to_string(),
+            }),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+#[repr(u8)]
+pub enum Restriction {
+    NoRestriction,
+    FillOrAbort,
+    ImmediateOrCancel,
+}
+
+impl TryFrom<u8> for Restriction {
+    type Error = TypeError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::NoRestriction),
+            1 => Ok(Self::FillOrAbort),
+            2 => Ok(Self::ImmediateOrCancel),
+            _ => Err(TypeError::ConversionError {
+                name: "Restriction".to_string(),
             }),
         }
     }

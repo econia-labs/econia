e1dd73d7f0af80cff8ebb7e96ebe6398d39ea3b7
bogberry <bogberry@users.noreply.github.com>
2023-04-25 22:47:49 +0900

    add get static props function

--- src/typescript/reference-frontend/src/pages/index.tsx
+++ src/typescript/reference-frontend/src/pages/index.tsx
@@ -1,19 +1,46 @@
-import { Jost } from "next/font/google";
-import Image from "next/image";
+import Head from "next/head";
 
-const jost = Jost({ subsets: ["latin"] });
+import { StatsBar } from "@/components/StatsBar";
+import { Layout } from "@/layouts/Layout";
+import { type ApiMarket } from "@/types/api";
 
-export default function Home() {
+export default function Home({ marketData }: { marketData: ApiMarket[] }) {
+  const marketNames: string[] = marketData
+    .sort((a, b) => a.market_id - b.market_id)
+    .map((market) => `${market.base.symbol}-${market.quote.symbol}`);
   return (
-    <main className={`h-screen bg-black ${jost.className}`}>
-      <Image
-        className="relative"
-        alt="Econia Logo"
-        src="/econia.svg"
-        width={156}
-        height={25}
-        priority
-      />
-    </main>
+    <>
+      <Head>
+        <title>Econia</title>
+      </Head>
+      <Layout>
+        <StatsBar />
+        <main>
+          <p className="font-roboto-mono text-white">
+            {JSON.stringify(marketNames)}
+          </p>
+        </main>
+      </Layout>
+    </>
   );
+}
+
+export async function getStaticProps() {
+  if (process.env.NEXT_PUBLIC_API_URL == null) {
+    throw new Error("NEXT_PUBLIC_API_URL not set");
+  }
+  const res = await fetch(
+    new URL("markets", process.env.NEXT_PUBLIC_API_URL).href
+  );
+  const marketData: ApiMarket[] = await res.json();
+
+  return {
+    props: {
+      marketData,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  };
 }

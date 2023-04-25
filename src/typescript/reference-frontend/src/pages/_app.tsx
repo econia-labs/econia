4357301453734cfeb4232d384fda33f1bb841fd4
bogberry <bogberry@users.noreply.github.com>
2023-04-25 19:06:21 +0900

    set up font imports

--- src/typescript/reference-frontend/src/pages/_app.tsx
+++ src/typescript/reference-frontend/src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
+import { Jost, Roboto_Mono } from "next/font/google";
+
+const jost = Jost({
+  subsets: ["latin"],
+  variable: "--font-jost",
+});
+
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${jost.variable} ${robotoMono.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }

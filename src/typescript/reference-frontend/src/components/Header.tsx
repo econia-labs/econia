91b0b78ea08dd972550fcdfd4cd64317ab7bf100
bogberry <bogberry@users.noreply.github.com>
2023-04-25 19:37:23 +0900

    add header element

---
+++ src/typescript/reference-frontend/src/components/Header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <header className="border-b border-neutral-600 bg-black">
+      <nav className="flex items-center justify-between px-8 py-4">
+        <div className="my-auto flex-1 items-center">
+          <Image
+            className=""
+            alt="Econia Logo"
+            src="/econia.svg"
+            width={120}
+            height={20}
+            priority
+          />
+        </div>
+        <div className="flex space-x-3">
+          <p className="font-lg font-roboto-mono font-light uppercase tracking-wide text-white hover:text-neutral-400">
+            Swap
+          </p>
+          <p className="font-lg font-roboto-mono font-light uppercase tracking-wide text-neutral-600">
+            /
+          </p>
+          <p className="font-lg font-roboto-mono font-light uppercase tracking-wide text-white hover:text-neutral-400">
+            Trade
+          </p>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <p className="font-roboto-mono text-white">Twitter</p>
+        </div>
+      </nav>
+    </header>
+  );
+}

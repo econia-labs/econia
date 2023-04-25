9b147d87baf9362a4753f5145a182d299c4b615b
bogberry <bogberry@users.noreply.github.com>
2023-04-25 17:18:25 +0900

    type checking for tailwind config

---
+++ src/typescript/reference-frontend/tailwind.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from "tailwindcss";
+
+const config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {},
+  plugins: [],
+} satisfies Config;
+
+export default config;

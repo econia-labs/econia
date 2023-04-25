735d9eebdfffe29a8ebb690ab1507e7275e9812e
bogberry <bogberry@users.noreply.github.com>
2023-04-25 17:02:33 +0900

    set up eslint

---
+++ src/typescript/reference-frontend/.eslintrc.cjs
@@ -0,0 +1,39 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+    "prettier",
+    "tailwindcss",
+    "simple-import-sort",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    ],
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
+  },
+};
+
+module.exports = config;

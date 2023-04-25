3f88b7e3c7bc3f8a0d23917d6373a90f6efa2148
bogberry <bogberry@users.noreply.github.com>
2023-04-25 22:50:18 +0900

    pass market names to stats bar

--- src/typescript/reference-frontend/src/components/StatsBar.tsx
+++ src/typescript/reference-frontend/src/components/StatsBar.tsx
@@ -1,10 +1,36 @@
-export function StatsBar() {
+import { Listbox } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+
+type Props = {
+  marketNames: string[];
+};
+
+export function StatsBar({ marketNames }: Props) {
+  const [selectedMarket, setSelectedMarket] = useState<string>(marketNames[0]);
+
   return (
-    <div className="flex border-b border-neutral-600 px-8 py-2">
+    <div className="flex border-b border-neutral-600 px-4 py-2">
       <div className="flex flex-1 items-center">
-        <div>
-          <p className="font-roboto-mono text-neutral-300">APT-USDC</p>
-        </div>
+        <Listbox value={selectedMarket} onChange={setSelectedMarket}>
+          <div className="relative w-[160px]">
+            <Listbox.Button className="flex px-4 font-roboto-mono text-neutral-300">
+              {selectedMarket}
+              <ChevronDownIcon className="my-auto ml-1 h-5 w-5 text-neutral-500" />
+            </Listbox.Button>
+            <Listbox.Options className="absolute mt-2 w-full bg-black shadow ring-1 ring-neutral-500">
+              {marketNames.map((marketName) => (
+                <Listbox.Option
+                  key={marketName}
+                  value={marketName}
+                  className="px-4 py-1 font-roboto-mono text-neutral-300 hover:bg-neutral-800"
+                >
+                  {marketName}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </div>
+        </Listbox>
         <div className="mb-1 ml-8">
           <span className="font-roboto-mono text-xs font-light uppercase text-neutral-400">
             Last price

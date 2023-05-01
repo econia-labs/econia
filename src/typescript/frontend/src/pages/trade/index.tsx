16ad86b8f625a53b9cce5167a41ed5dfcf30d148
Brian Li <brnli7@gmail.com>
2023-05-01 19:34:08 -0400

    Use Page for tradex/index.tsx

--- src/typescript/frontend/src/pages/trade/index.tsx
+++ src/typescript/frontend/src/pages/trade/index.tsx
@@ -3,6 +3,7 @@ import { type GetStaticProps } from "next";
 import { Header } from "@/components/Header";
 import { StatsBar } from "@/components/StatsBar";
 import { type ApiMarket } from "@/types/api";
+import { Page } from "@/components/Page";
 
 export default function Trade({ marketData }: { marketData: ApiMarket[] }) {
   const marketNames: string[] = marketData
@@ -12,23 +13,20 @@ export default function Trade({ marketData }: { marketData: ApiMarket[] }) {
   marketNames.push("APT-USDC", "ETH-USDC");
 
   return (
-    <>
-      <div className="flex min-h-screen w-full flex-col bg-black">
-        <Header />
-        <StatsBar marketNames={marketNames} />
-        <main className="flex flex-1">
-          <div className="flex-1 border-r border-neutral-600 px-4 py-2">
-            <p className="font-jost text-white">Price Chart</p>
-          </div>
-          <div className="w-[320px] flex-initial border-r border-neutral-600 px-4 py-2">
-            <p className="font-jost text-white">Orderbook</p>
-          </div>
-          <div className="w-[320px] flex-initial px-4 py-2">
-            <p className="font-jost text-white">Order Entry</p>
-          </div>
-        </main>
-      </div>
-    </>
+    <Page>
+      <StatsBar marketNames={marketNames} />
+      <main className="flex flex-1">
+        <div className="flex-1 border-r border-neutral-600 px-4 py-2">
+          <p className="font-jost text-white">Price Chart</p>
+        </div>
+        <div className="w-[320px] flex-initial border-r border-neutral-600 px-4 py-2">
+          <p className="font-jost text-white">Orderbook</p>
+        </div>
+        <div className="w-[320px] flex-initial px-4 py-2">
+          <p className="font-jost text-white">Order Entry</p>
+        </div>
+      </main>
+    </Page>
   );
 }
 

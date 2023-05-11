92a21012aea4c96b44edd990c1369c70cdeff8f1
bogberry <bogberry@users.noreply.github.com>
2023-05-11 13:24:27 +0900

    fix import order

--- src/typescript/frontend/tailwind.config.ts
+++ src/typescript/frontend/tailwind.config.ts
@@ -1,6 +1,6 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 import colors from "tailwindcss/colors";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
   content: [

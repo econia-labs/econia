41b8339308d5cffc699a623853480020acc95d14
Brian Li <brnli7@gmail.com>
2023-05-11 01:17:55 -0400

    Update Button style (#179)
    * Add Figma colors
    
    * Fix build
    
    * Update Header
    
    * Add Link to logo
    
    * Update Button style
    
    * Remove unused ConnectWalletButton
    
    ---------
    
    Co-authored-by: bogberry <bogberry@users.noreply.github.com>

--- src/typescript/frontend/src/components/Button.tsx
+++ src/typescript/frontend/src/components/Button.tsx
@@ -1,14 +1,20 @@
-export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (
-  props
-) => {
+export const Button: React.FC<
+  React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    variant: "primary" | "outlined";
+  }
+> = ({ variant, ...props }) => {
+  const variantStyle =
+    variant === "primary"
+      ? "bg-neutral-100 text-neutral-800"
+      : "bg-neutral-800 text-neutral-100 border border-neutral-100";
   return (
     <button
       {...props}
-      className={
-        "w-full bg-white p-3 text-left text-center text-lg font-semibold uppercase text-black" +
-        " " +
-        props.className
-      }
+      className={[
+        "w-full px-4 py-2 text-center font-jost text-lg font-semibold",
+        variantStyle,
+        props.className,
+      ].join(" ")}
     />
   );
 };
--- src/typescript/frontend/src/pages/faucet/index.tsx
+++ src/typescript/frontend/src/pages/faucet/index.tsx
@@ -10,7 +10,9 @@ const FaucetCard: React.FC<{ symbol: string }> = ({ symbol }) => {
     <div className="flex h-48 w-96 flex-col items-center justify-center border p-8">
       <h1 className="font-jost text-xl font-bold text-white">{symbol}</h1>
       <p className="font-jost text-gray-400">Balance: -- {symbol}</p>
-      <Button className="mt-4">Get {symbol}</Button>
+      <Button variant="primary" className="mt-4">
+        Get {symbol}
+      </Button>
     </div>
   );
 };

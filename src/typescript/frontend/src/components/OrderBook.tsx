b144f361415f6e5bcfff91411cbd20cc98cbefb8
chiumax <dumblole@gmail.com>
2023-05-15 21:27:26 -0400

    overall structure done

--- src/typescript/frontend/src/components/OrderBook.tsx
+++ src/typescript/frontend/src/components/OrderBook.tsx
@@ -2,22 +2,23 @@ import { Listbox } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { useEffect, useState } from "react";
 import { ApiMarket } from "@/types/api";
+import { set } from "react-hook-form";
 
 type OrderPrice = {
   price: number;
   amount: number;
-  volume: number;
+  depth: number;
 };
 
 const generateMockData = (): { asks: OrderPrice[]; bids: OrderPrice[] } => {
   const asks: OrderPrice[] = [];
   const bids: OrderPrice[] = [];
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 30; i++) {
     const price = Math.floor(Math.random() * 100);
     const amount = Math.floor(Math.random() * 10);
-    const volume = Math.floor(Math.random() * 100);
-    const order = { price, amount, volume };
+    const depth = Math.floor(Math.random() * 100);
+    const order = { price, amount, depth };
 
     if (i % 2 === 0) {
       asks.push(order);
@@ -32,6 +33,9 @@ const generateMockData = (): { asks: OrderPrice[]; bids: OrderPrice[] } => {
 export function OrderBook({ marketData }: { marketData: ApiMarket }) {
   const [asks, setAsks] = useState<OrderPrice[]>([]);
   const [bids, setBids] = useState<OrderPrice[]>([]);
+  const [spread, setSpread] = useState<OrderPrice>();
+
+  const [selectedMarket, setSelectedMarket] = useState<string>("0.01");
 
   useEffect(() => {
     // use mock data if no market data is provided
@@ -39,6 +43,11 @@ export function OrderBook({ marketData }: { marketData: ApiMarket }) {
       const mockData = generateMockData();
       setAsks(mockData.asks);
       setBids(mockData.bids);
+      setSpread({
+        price: mockData.asks[0].price - mockData.bids[0].price,
+        amount: 10,
+        depth: 0,
+      });
     }
   }, []);
 
@@ -60,17 +69,43 @@ export function OrderBook({ marketData }: { marketData: ApiMarket }) {
           type === "sell" ? "bg-red-400" : "bg-green"
         }`}
         // dynamic taillwind?
-        style={{ width: `${order.volume}%` }}
+        style={{ width: `${order.depth}%` }}
       ></div>
     </div>
   );
 
   return (
-    <div>
+    <div className="divider-solid divide-y divide-neutral-600">
       {/* title row */}
-      <div>
-        <p className={"ml-4 mt-2 font-jost text-white"}>Order Book</p>
-        <p>granularity dropdown</p>
+      <div className={"mx-4 my-[12px]"}>
+        <div className={"flex justify-between"}>
+          <p className={"font-jost text-white"}>Order Book</p>
+          <Listbox value={selectedMarket} onChange={setSelectedMarket}>
+            <div className="relative min-h-[30px]  border border-neutral-600 px-[11px] py-[8px]">
+              <Listbox.Button className="flex  font-roboto-mono text-neutral-300">
+                {selectedMarket}
+                <ChevronDownIcon className="my-auto ml-1 h-5 w-5 text-neutral-500" />
+              </Listbox.Button>
+              <Listbox.Options className="absolute mt-2 w-full bg-black shadow ring-1 ring-neutral-500">
+                {[].map((marketName, i) => (
+                  <Listbox.Option
+                    key={i}
+                    value={marketName}
+                    className="weight-300 px-4 py-1 font-roboto-mono text-xs text-neutral-300 hover:bg-neutral-800"
+                  >
+                    {marketName}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </div>
+          </Listbox>
+        </div>
+        <div
+          className={`mt-[11px] flex justify-between text-xs text-neutral-500`}
+        >
+          <div className={``}>PRICE (USD)</div>
+          <div>AMOUNT (APT)</div>
+        </div>
       </div>
       {/* ASK */}
       <div>
@@ -78,9 +113,12 @@ export function OrderBook({ marketData }: { marketData: ApiMarket }) {
           <Row order={order} type={"sell"} key={"ask" + index} />
         ))}
       </div>
-      {/* equilibrium */}
-      <div className="flex items-center justify-center bg-gray-300 text-sm font-bold">
-        Equilibrium
+      {/* SPREAD */}
+      <div className="flex min-h-[40px] items-center justify-between text-xs ">
+        <div className={`z-10 ml-4 text-right text-white`}>
+          {spread?.price || "-"}
+        </div>
+        <div className="z-10 mr-4 text-white">{spread?.amount || "-"}</div>
       </div>
       {/* BID */}
       <div>

8c2ec7fc5b7fe33d68647150e03c60d260473528
Bogberry <127273034+bogberry@users.noreply.github.com>
2023-05-22 15:45:36 +0900

    Use typescript sdk in frontend (#198)
    * add sdk as dependency
    
    * refactor into wrapper component
    
    * remove arrows from number input
    
    * add basic form validation
    
    * submit transaction
    
    * upgrade deps
    
    * Working module import
    
    * don't use .d.ts files for now
    
    * use snake case
    
    * fix entry function types
    
    * Add preinstall hook to build the sdk project
    
    * use latest versions of dependencies
    
    * needs to be preinstall
    
    ---------
    
    Co-authored-by: bogberry <bogberry@users.noreply.github.com>
    Co-authored-by: Brian Li <brnli7@gmail.com>

--- src/typescript/frontend/src/components/trade/OrderEntry/LimitOrderEntry.tsx
+++ src/typescript/frontend/src/components/trade/OrderEntry/LimitOrderEntry.tsx
@@ -1,21 +1,37 @@
-import { ApiMarket } from "@/types/api";
-import { OrderEntryInput } from "@/components/trade/OrderEntry/OrderEntryInput";
-import { type Side } from "@/types/global";
+import { entryFunctions, type order } from "@econia-labs/sdk";
+import { useWallet } from "@manahippo/aptos-wallet-adapter";
+import { useForm } from "react-hook-form";
+
 import { Button } from "@/components/Button";
 import { ConnectedButton } from "@/components/ConnectedButton";
-import { useState } from "react";
-import { OrderEntryInfo } from "./OrderEntryInfo";
-import { useWallet } from "@manahippo/aptos-wallet-adapter";
+import { useAptos } from "@/contexts/AptosContext";
 import { useCoinBalance } from "@/hooks/useCoinBalance";
+import { type ApiMarket } from "@/types/api";
+import { type Side } from "@/types/global";
 import { TypeTag } from "@/types/move";
+
+import { OrderEntryInfo } from "./OrderEntryInfo";
+import { OrderEntryInputWrapper } from "./OrderEntryInputWrapper";
+
+type LimitFormValues = {
+  price: string;
+  size: string;
+  totalSize: string;
+};
 
 export const LimitOrderEntry: React.FC<{
   marketData: ApiMarket;
   side: Side;
 }> = ({ marketData, side }) => {
-  const { account } = useWallet();
-  const [price, setPrice] = useState<string>("");
-  const [amount, setAmount] = useState<string>("");
+  const { aptosClient } = useAptos();
+  const { account, signAndSubmitTransaction } = useWallet();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    getValues,
+    setValue,
+  } = useForm<LimitFormValues>();
   const baseBalance = useCoinBalance(
     marketData.base ? TypeTag.fromApiCoin(marketData.base) : null,
     account?.address
@@ -25,45 +41,116 @@ export const LimitOrderEntry: React.FC<{
     account?.address
   );
 
+  const onSubmit = async (values: LimitFormValues) => {
+    console.log(values);
+
+    const orderSideMap: Record<Side, order.Side> = {
+      buy: "bid",
+      sell: "ask",
+    };
+    const orderSide = orderSideMap[side];
+
+    if (marketData.base == null) {
+      // TODO: handle generic markets
+    } else {
+      const payload = entryFunctions.placeLimitOrderUserEntry(
+        "0xeconia", // TODO pass in econia address as environment variable
+        `${marketData.base.account_address}::${marketData.base.module_name}::${marketData.base.struct_name}`,
+        `${marketData.quote.account_address}::${marketData.quote.module_name}::${marketData.quote.struct_name}`,
+        BigInt(marketData.market_id), // market id
+        "0x1", // TODO get integrator ID
+        orderSide,
+        BigInt(values.price),
+        BigInt(values.size),
+        "immediateOrCancel", // TODO don't hardcode
+        "abort" // don't hardcode this either
+      );
+
+      const { hash } = await signAndSubmitTransaction({
+        type: "entry_function_payload",
+        ...payload,
+      });
+      await aptosClient.waitForTransaction(hash, { checkSuccess: true });
+    }
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mx-4">
-        <OrderEntryInput
-          value={price}
-          onChange={setPrice}
+        <OrderEntryInputWrapper
           startAdornment="LIMIT PRICE"
           endAdornment={marketData.quote.symbol}
-          type="number"
-          placeholder="0.00"
-        />
+        >
+          <input
+            type="number"
+            step="any"
+            {...register("price", {
+              required: "required",
+              min: 0,
+              // TODO: check that amount * size does not exceed quote currency
+              // balance for bids
+              onChange: (e) => {
+                const size = Number(getValues("size"));
+                if (!isNaN(size) && !isNaN(e.target.value)) {
+                  const totalSize = (size * e.target.value).toFixed(4);
+                  setValue("totalSize", totalSize);
+                } else {
+                  setValue("totalSize", "");
+                }
+              },
+            })}
+            className="h-full w-[100px] flex-1 bg-transparent text-right font-roboto-mono font-light text-neutral-400 outline-none"
+          />
+        </OrderEntryInputWrapper>
+        <p className="text-red">
+          {errors.price != null && errors.price.message}
+        </p>
       </div>
       <hr className="my-4 border-neutral-600" />
       <div className="mx-4 flex flex-col gap-4">
-        <OrderEntryInput
-          value={amount}
-          onChange={setAmount}
+        <OrderEntryInputWrapper
           startAdornment="AMOUNT"
           endAdornment={marketData.base?.symbol}
-          type="number"
-          placeholder="0.00"
-        />
-        <OrderEntryInput
-          value={(
-            parseFloat(price === "" ? "0" : price) *
-            parseFloat(amount === "" ? "0" : amount)
-          ).toFixed(4)}
+        >
+          <input
+            type="number"
+            step="any"
+            {...register("size", {
+              required: "required",
+              min: 0,
+              // TODO: check that size does not exceed base currency balance for asks
+              onChange: (e) => {
+                const price = Number(getValues("price"));
+                if (!isNaN(price) && !isNaN(e.target.value)) {
+                  const totalSize = (price * e.target.value).toFixed(4);
+                  setValue("totalSize", totalSize);
+                } else {
+                  setValue("totalSize", "");
+                }
+              },
+            })}
+            className="h-full w-[100px] flex-1 bg-transparent text-right font-roboto-mono font-light text-neutral-400 outline-none"
+          />
+        </OrderEntryInputWrapper>
+        <p className="text-red">{errors.size != null && errors.size.message}</p>
+        <OrderEntryInputWrapper
           startAdornment="TOTAL"
           endAdornment={marketData.quote?.symbol}
-          type="number"
-          placeholder="0.00"
-          disabled
-        />
+        >
+          <input
+            type="number"
+            step="any"
+            {...register("totalSize", { disabled: true })}
+            className="h-full w-[100px] flex-1 bg-transparent text-right font-roboto-mono font-light text-neutral-400 outline-none"
+          />
+        </OrderEntryInputWrapper>
       </div>
       <hr className="my-4 border-neutral-600" />
       <div className="mx-4 mb-4 flex flex-col gap-4">
         <OrderEntryInfo label="EST. FEE" value="--" />
         <ConnectedButton className="w-full">
           <Button
+            type="submit"
             variant={side === "buy" ? "green" : "red"}
             className={`w-full`}
           >
@@ -79,6 +166,6 @@ export const LimitOrderEntry: React.FC<{
           value={`${quoteBalance.data ?? "--"} ${marketData.quote?.symbol}`}
         />
       </div>
-    </>
+    </form>
   );
 };

e2c9c1f6e01caeb188d1e8bcf793f4e5bec49f81
chiumax <dumblole@gmail.com>
2023-05-22 13:46:52 -0400

    updating types

--- src/typescript/frontend/src/components/DepthChart.tsx
+++ src/typescript/frontend/src/components/DepthChart.tsx
@@ -1,282 +1,233 @@
-import type AxisRendererProps from "@visx/axis/lib/axis/AxisRenderer";
-import type Scale from "@visx/axis/lib/axis/AxisRenderer";
-import type AxisScale from "@visx/axis/lib/types";
-import { Axis as AxisLib, XYChart as XYChartLib } from "@visx/visx";
-import { type ReactNode } from "react";
+import { ApiMarket } from "@/types/api";
+import BigNumber from "bignumber.js";
+import { useMemo } from "react";
+import { Line } from "react-chartjs-2";
 
-const data1 = [
-  { x: 0, y: 50 },
-  { x: 1, y: 10 },
-  { x: 2, y: 30 },
-];
+export const ZERO_BIGNUMBER = new BigNumber(0);
+export const DepthChart: React.FC<{
+  market: RegisteredMarket;
+  marketData: ApiMarket | undefined;
+}> = ({ market, marketData }) => {
+  const baseCoinInfo = marketData?.base;
+  const quoteCoinInfo = marketData?.quote;
+  const orderBook = useOrderBook(market.marketId);
+  const { labels, bidData, askData, minPrice, maxPrice } = useMemo(() => {
+    const labels: number[] = [];
+    const bidData: (number | undefined)[] = [];
+    const askData: (number | undefined)[] = [];
+    let minPrice = Infinity;
+    let maxPrice = -Infinity;
+    if (orderBook.data) {
+      // Get min and max price to set a range
+      for (const order of orderBook.data.bids.concat(orderBook.data.asks)) {
+        if (order.price.toJsNumber() < minPrice) {
+          minPrice = order.price.toJsNumber();
+        }
+        if (order.price.toJsNumber() > maxPrice) {
+          maxPrice = order.price.toJsNumber();
+        }
+      }
 
-// for (let i = 3; i < 103; i++) {
-//   const newDataPoint = { x: i, y: Math.floor(Math.random() * 100) };
-//   data1.push(newDataPoint);
-// }
-const data2 = [
-  { x: 2, y: 30 },
-  { x: 3, y: 40 },
-  { x: 4, y: 80 },
-];
+      // Append prices in ascending order to `labels`
+      orderBook.data.bids
+        .slice()
+        .concat(orderBook.data.asks.slice())
+        .sort(
+          (
+            a: { price: { toJsNumber: () => number } },
+            b: { price: { toJsNumber: () => number } }
+          ) => a.price.toJsNumber() - b.price.toJsNumber()
+        )
+        .forEach((o: { price: { toJsNumber: () => number } }) => {
+          labels.push(o.price.toJsNumber());
+          bidData.push(undefined);
+          askData.push(undefined);
+        });
 
-// const data2 = [
-//   { x: 103, y: 30 },
-//   { x: 104, y: 40 },
-//   { x: 105, y: 80 },
-// ];
-// for (let i = 105; i < 205; i++) {
-//   const newDataPoint = { x: i, y: Math.floor(Math.random() * 100) };
-//   data2.push(newDataPoint);
-// }
+      const bidPriceToSize = new Map<number, number>();
+      const askPriceToSize = new Map<number, number>();
+      for (const { price, size } of orderBook.data.bids) {
+        const priceKey = price.toJsNumber();
+        if (!bidPriceToSize.has(priceKey)) {
+          bidPriceToSize.set(priceKey, 0);
+        }
+        bidPriceToSize.set(
+          priceKey,
+          bidPriceToSize.get(priceKey)! + size.toJsNumber()
+        );
+      }
+      for (const { price, size } of orderBook.data.asks) {
+        const priceKey = price.toJsNumber();
+        if (!askPriceToSize.has(priceKey)) {
+          askPriceToSize.set(priceKey, 0);
+        }
+        askPriceToSize.set(
+          priceKey,
+          askPriceToSize.get(priceKey)! + size.toJsNumber()
+        );
+      }
 
-const accessors = {
-  xAccessor: (d: { x: number }) => d.x,
-  yAccessor: (d: { y: number }) => d.y,
-};
-type xyDatum = { x: string; y: number };
-type xDatum = { x: number };
-type yDatum = { y: number };
+      let askAcc = ZERO_BIGNUMBER;
+      for (let i = 0; i < labels.length; i++) {
+        const price = labels[i];
+        if (askPriceToSize.has(price))
+          askAcc = askAcc.plus(askPriceToSize.get(price)!);
+        if (askAcc.gt(0))
+          askData[i] = toDecimalSize({
+            size: askAcc,
+            lotSize: market.lotSize,
+            baseCoinDecimals: BigNumber(baseCoinInfo?.decimals || 0),
+          }).toNumber();
+      }
 
-export const DepthChart = () => {
-  const {
-    Axis,
-    Grid,
-    AreaSeries,
-    Tooltip,
-    XYChart,
-    darkTheme,
-    AreaStack,
-    buildChartTheme,
-  } = XYChartLib;
-  const customTheme = buildChartTheme({
-    // colors
-    backgroundColor: "#000", // used by Tooltip, Annotation
-    colors: ["rgba(110, 213, 163, 1)", "rgba(213, 110, 110, 1)"], // categorical colors, mapped to series via `dataKey`s
-    tickLength: 4,
-    svgLabelSmall: {
-      fill: "white",
-    },
-    svgLabelBig: {
-      fill: "white",
-    },
-    gridColor: "white",
-    gridColorDark: "white",
-    // labels
-    // svgLabelBig?: SVGTextProps;
-    // svgLabelSmall?: SVGTextProps;
-    // htmlLabel?: HTMLTextStyles;
+      // We go in reverse order to get the accumulated bid size
+      let bidAcc = ZERO_BIGNUMBER;
+      for (let i = labels.length - 1; i >= 0; i--) {
+        const price = labels[i];
+        if (bidPriceToSize.has(price))
+          bidAcc = bidAcc.plus(bidPriceToSize.get(price)!);
+        if (bidAcc.gt(0))
+          bidData[i] = toDecimalSize({
+            size: bidAcc,
+            lotSize: market.lotSize,
+            baseCoinDecimals: BigNumber(baseCoinInfo?.decimals || 0),
+          }).toNumber();
+      }
 
-    // lines
-    // xAxisLineStyles?: LineStyles;
-    // yAxisLineStyles?: LineStyles;
-    // xTickLineStyles?: LineStyles;
-    // yTickLineStyles?: LineStyles;
-    // tickLength: number;
+      labels.forEach((price, i) => {
+        labels[i] = toDecimalPrice({
+          price: new BigNumber(price),
+          lotSize: market.lotSize,
+          tickSize: market.tickSize,
+          baseCoinDecimals: BigNumber(baseCoinInfo?.decimals || 0),
+          quoteCoinDecimals: BigNumber(quoteCoinInfo?.decimals || 0),
+        }).toNumber();
+      });
+    }
+    return {
+      labels,
+      bidData,
+      askData,
+      minPrice: toDecimalPrice({
+        price: new BigNumber(minPrice),
+        lotSize: market.lotSize,
+        tickSize: market.tickSize,
+        baseCoinDecimals: BigNumber(baseCoinInfo?.decimals || 0),
+        quoteCoinDecimals: BigNumber(quoteCoinInfo?.decimals || 0),
+      }),
+      maxPrice: toDecimalPrice({
+        price: new BigNumber(maxPrice),
+        lotSize: market.lotSize,
+        tickSize: market.tickSize,
+        baseCoinDecimals: BigNumber(baseCoinInfo?.decimals || 0),
+        quoteCoinDecimals: BigNumber(quoteCoinInfo?.decimals || 0),
+      }),
+    };
+  }, [market, baseCoinInfo, quoteCoinInfo, orderBook.data]);
 
-    // grid
-    // gridColor: string;
-    // gridColorDark: string; // used for axis baseline if x/yxAxisLineStyles not set
-    // gridStyles?: CSSProperties;
-  });
   return (
-    <XYChart
-      height={300}
-      xScale={{ type: "band" }}
-      yScale={{ type: "linear" }}
-      theme={customTheme}
-    >
-      <Axis orientation="bottom" hideTicks />
-      <Axis orientation="right" hideTicks></Axis>
-      {/* <Grid columns={false} numTicks={4} /> */}
-
-      <AreaSeries
-        dataKey="Line 1"
-        data={data1}
-        {...accessors}
-        fillOpacity={0.4}
-      />
-      <AreaSeries
-        dataKey="Line 2"
-        data={data2}
-        {...accessors}
-        fillOpacity={0.4}
-      />
-
-      <Tooltip
-        snapTooltipToDatumX
-        snapTooltipToDatumY
-        showVerticalCrosshair
-        showHorizontalCrosshair
-        verticalCrosshairStyle={{
-          stroke: "#fff",
-          strokeDasharray: "2,2",
-        }}
-        horizontalCrosshairStyle={{
-          stroke: "#fff",
-          strokeDasharray: "2,2",
-        }}
-        // applyPositionStyle
-        // unstyled
-        showSeriesGlyphs={false}
-        renderTooltip={({ tooltipData, colorScale }) => {
-          if (!tooltipData) return null;
-          if (!tooltipData.nearestDatum) return null;
-          if (!colorScale) return null;
-
-          return (
-            <div>
-              {[tooltipData?.nearestDatum?.key]
-                .filter((city) => city)
-                .map((city) => {
-                  return <div key={city}>test</div>;
-                })}
-              <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
-                {/* <div style={{ color: "Red" }}> */}
-                {tooltipData.nearestDatum.key}
-              </div>
-              {accessors.xAccessor(tooltipData.nearestDatum.datum as xDatum)}
-              {", "}
-              {accessors.yAccessor(tooltipData.nearestDatum.datum as yDatum)}
-            </div>
-          );
-        }}
-      />
-    </XYChart>
+    <Line
+      options={{
+        responsive: true,
+        maintainAspectRatio: false,
+        interaction: {
+          intersect: false,
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            // style tooltip to match the theme
+            callbacks: {
+              label: (item: { label: any; raw: any }) => {
+                return [
+                  `Price: ${item.label} ${quoteCoinInfo?.symbol}`,
+                  `Total Size: ${item.raw} ${baseCoinInfo?.symbol}`,
+                ];
+              },
+              title: () => "",
+            },
+            displayColors: false,
+            bodyAlign: "right",
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: true,
+              color: "gray",
+              // color: theme.colors.grey[700],
+            },
+            // We don't use linear for now because it doesn't ensure that the graph fits nicely in tick sizes
+            // type: "linear",
+            // ticks: {
+            //   stepSize,
+            // },
+          },
+          y: {
+            grid: {
+              display: true,
+              color: "gray",
+            },
+            beginAtZero: true,
+          },
+        },
+      }}
+      data={{
+        labels,
+        datasets: [
+          {
+            fill: true,
+            label: "Size",
+            data: bidData,
+            borderColor: "green",
+            backgroundColor: "green" + "44",
+            stepped: true,
+          },
+          {
+            fill: true,
+            label: "Size",
+            data: askData,
+            borderColor: "red",
+            backgroundColor: "red" + "44",
+            stepped: true,
+          },
+        ],
+      }}
+    />
   );
 };
 
-/** wip for coinbase-like tooltip */
+const TEN = new BigNumber(10);
+export const toDecimalPrice = ({
+  price,
+  lotSize,
+  tickSize,
+  baseCoinDecimals,
+  quoteCoinDecimals,
+}: {
+  price: BigNumber;
+  lotSize: BigNumber;
+  tickSize: BigNumber;
+  baseCoinDecimals: BigNumber;
+  quoteCoinDecimals: BigNumber;
+}) => {
+  const lotsPerUnit = TEN.exponentiatedBy(baseCoinDecimals).div(lotSize);
+  const pricePerLot = price
+    .multipliedBy(tickSize)
+    .div(TEN.exponentiatedBy(quoteCoinDecimals));
+  return pricePerLot.multipliedBy(lotsPerUnit);
+};
 
-/**
- * 
-
-
- <XYChart
-      height={300}
-      xScale={{ type: "band" }}
-      yScale={{ type: "linear" }}
-      theme={darkTheme}
-    >
-      <Axis orientation="bottom" hideTicks />
-      <Axis orientation="right" hideTicks>
-        {({
-          axisFromPoint,
-          axisLineClassName,
-          axisToPoint,
-          hideAxisLine,
-          hideTicks,
-          horizontal,
-          label = "",
-          labelClassName,
-          labelOffset = 14,
-          labelProps = "defaultTextProps",
-          orientation = Orientation.bottom,
-          scale,
-          stroke = "#222",
-          strokeDasharray,
-          strokeWidth = 1,
-          tickClassName,
-          tickComponent,
-          tickLineProps,
-          tickLabelProps,
-          tickLength = 8,
-          tickStroke = "#222",
-          tickTransform,
-          ticks,
-          ticksComponent = Ticks,
-        }: props) => {
-          const tickLabelPropsDefault = {
-            ...defaultTextProps,
-            ...(typeof tickLabelProps === "object" ? tickLabelProps : null),
-          };
-          // compute the max tick label size to compute label offset
-          const allTickLabelProps = ticks.map(({ value, index }) =>
-            typeof tickLabelProps === "function"
-              ? tickLabelProps(value, index, ticks)
-              : tickLabelPropsDefault
-          );
-          const maxTickLabelFontSize = Math.max(
-            10,
-            ...allTickLabelProps.map((props) =>
-              typeof props.fontSize === "number" ? props.fontSize : 0
-            )
-          );
-          return (
-            <>
-              {ticksComponent({
-                hideTicks,
-                horizontal,
-                orientation,
-                scale,
-                tickClassName,
-                tickComponent,
-                tickLabelProps: allTickLabelProps,
-                tickStroke,
-                tickTransform,
-                ticks,
-                strokeWidth,
-                tickLineProps,
-              })}
-
-              {!hideAxisLine && (
-                <Line
-                  className={cx("visx-axis-line", axisLineClassName)}
-                  from={axisFromPoint}
-                  to={axisToPoint}
-                  stroke={stroke}
-                  strokeWidth={strokeWidth}
-                  strokeDasharray={strokeDasharray}
-                />
-              )}
-
-              {label && (
-                <Text
-                  className={cx("visx-axis-label", labelClassName)}
-                  {...getLabelTransform({
-                    labelOffset,
-                    labelProps,
-                    orientation,
-                    range: scale.range(),
-                    tickLabelFontSize: maxTickLabelFontSize,
-                    tickLength,
-                  })}
-                  {...labelProps}
-                >
-                  {label}
-                </Text>
-              )}
-            </>
-          );
-        }}
-      </Axis>
-      <Grid columns={false} numTicks={4} />
-      <AreaSeries dataKey="Line 1" data={data1} {...accessors} />
-      <Tooltip
-        snapTooltipToDatumX
-        snapTooltipToDatumY
-        showVerticalCrosshair
-        showHorizontalCrosshair
-        applyPositionStyle
-        // unstyled
-        showSeriesGlyphs
-        renderTooltip={({ tooltipData, colorScale }) => {
-          if (!tooltipData) return null;
-          if (!tooltipData.nearestDatum) return null;
-          if (!colorScale) return null;
-          return (
-            <div>
-              <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
-                {tooltipData.nearestDatum.key}
-              </div>
-              {accessors.xAccessor(tooltipData.nearestDatum.datum as xDatum)}
-              {", "}
-              {accessors.yAccessor(tooltipData.nearestDatum.datum as yDatum)}
-            </div>
-          );
-        }}
-      />
-    </XYChart>
-
-
- */
+export const toDecimalSize = ({
+  size,
+  lotSize,
+  baseCoinDecimals,
+}: {
+  size: BigNumber;
+  lotSize: BigNumber;
+  baseCoinDecimals: BigNumber;
+}) => {
+  return size.multipliedBy(lotSize).div(TEN.exponentiatedBy(baseCoinDecimals));
+};

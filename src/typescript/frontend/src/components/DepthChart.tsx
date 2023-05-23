57d05754ec0bc124753aa977e757ce63fbba8eeb
chiumax <dumblole@gmail.com>
2023-05-23 09:57:47 -0400

    cleanup

--- src/typescript/frontend/src/components/DepthChart.tsx
+++ src/typescript/frontend/src/components/DepthChart.tsx
@@ -247,10 +247,6 @@ export const DepthChart: React.FC<{
             y: {
               position: "right",
               max: Math.max(bidData[0] || 0, askData[askData.length - 1] || 0),
-              // grid: {
-              //   display: true,
-              //   color: "gray",
-              // },
               ticks: {
                 maxTicksLimit: 2,
               },
@@ -388,46 +384,3 @@ const plugin = {
   },
 };
 Chart.register(plugin);
-
-// const corsairPlugin: Chart.PluginServiceRegistrationOptions<CorsairPluginOptions> =
-//   {
-//     id: "corsair",
-//     defaults: {
-//       width: 1,
-//       color: "#FF4949",
-//       dash: [3, 3],
-//     },
-//     afterInit: (chart, args, opts) => {
-//       chart.corsair = {
-//         x: 0,
-//         y: 0,
-//       };
-//     },
-//     afterEvent: (chart, args) => {
-//       const { inRange } = args;
-//       const { type, x, y } = args.event;
-
-//       chart.corsair = { x, y, draw: inRange };
-//       chart.update();
-//     },
-//     beforeDatasetsDraw: (chart, args, opts) => {
-//       const { ctx } = chart;
-//       const { top, bottom, left, right } = chart.chartArea;
-//       const { x, y, draw } = chart.corsair;
-//       if (!draw) return;
-
-//       ctx.save();
-
-//       ctx.beginPath();
-//       ctx.lineWidth = opts.width;
-//       ctx.strokeStyle = opts.color;
-//       ctx.setLineDash(opts.dash);
-//       ctx.moveTo(x, bottom);
-//       ctx.lineTo(x, top);
-//       ctx.moveTo(left, y);
-//       ctx.lineTo(right, y);
-//       ctx.stroke();
-
-//       ctx.restore();
-//     },
-//   };

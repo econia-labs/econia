e7c0f6c8f0181020f87414aa04ad36186edd7da8
Brian Li <brnli7@gmail.com>
2023-05-25 01:59:41 -0400

    Enable toasts + toast style (#218)

--- src/typescript/frontend/src/pages/_app.tsx
+++ src/typescript/frontend/src/pages/_app.tsx
@@ -13,6 +13,8 @@ import { useMemo } from "react";
 
 import { AptosContextProvider } from "@/contexts/AptosContext";
 import { ConnectWalletContextProvider } from "@/contexts/ConnectWalletContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const jost = Jost({
   subsets: ["latin"],
@@ -40,6 +42,24 @@ export default function App({ Component, pageProps }: AppProps) {
               :root {
                 --font-jost: ${jost.style.fontFamily};
                 --font-roboto-mono: ${robotoMono.style.fontFamily};
+                --toastify-color: #020202;
+                --toastify-color-info: #62c6f8;
+                --toastify-color-success: #6ed5a3;
+                --toastify-color-error: #d56e6e;
+                --toastify-color-warning: #eef081;
+
+                --toastify-icon-color-info: #62c6f8;
+                --toastify-icon-color-success: #6ed5a3;
+                --toastify-icon-color-error: #d56e6e;
+                --toastify-icon-color-warning: #eef081;
+
+                --toastify-font-family: ${robotoMono.style.fontFamily};
+                --toastify-color-dark: #020202;
+                --toastify-toast-background: #020202;
+              }
+              .Toastify__toast {
+                border: 1px solid #565656;
+                border-radius: 0px;
               }
             `}</style>
             <div>
@@ -49,6 +69,7 @@ export default function App({ Component, pageProps }: AppProps) {
         </AptosContextProvider>
       </WalletProvider>
       <ReactQueryDevtools initialIsOpen={false} />
+      <ToastContainer theme="dark" />
     </QueryClientProvider>
   );
 }

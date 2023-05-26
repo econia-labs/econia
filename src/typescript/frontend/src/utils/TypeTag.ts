59291956495b72b594f5bf7a1624913f0eb1f947
Brian Li <brnli7@gmail.com>
2023-05-26 11:19:43 -0400

    Show market account balance in deposit/withdraw modal
    Also refactor types into respective files and move TypeTag into utils/

---
+++ src/typescript/frontend/src/utils/TypeTag.ts
@@ -0,0 +1,39 @@
+import { ECONIA_ADDR } from "@/env";
+import { ApiCoin } from "@/types/api";
+import { MovePrimitive } from "@/types/move";
+
+export class TypeTag {
+  constructor(
+    public addr: string,
+    public module: string,
+    public name: string
+  ) {}
+
+  static fromApiCoin(apiCoin: ApiCoin) {
+    return new TypeTag(
+      apiCoin.account_address,
+      apiCoin.module_name,
+      apiCoin.struct_name
+    );
+  }
+
+  static fromTablistNode(tablistNode: {
+    key: MovePrimitive;
+    value: string | TypeTag;
+  }) {
+    return new TypeTag(
+      ECONIA_ADDR,
+      "tablist",
+      `Node<${tablistNode.key},${tablistNode.value.toString()}>`
+    );
+  }
+
+  static fromString(typeTag: string) {
+    const [addr, module, name] = typeTag.split("::");
+    return new TypeTag(addr, module, name);
+  }
+
+  toString() {
+    return `${this.addr}::${this.module}::${this.name}`;
+  }
+}

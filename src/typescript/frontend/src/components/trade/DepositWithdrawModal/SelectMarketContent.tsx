ee7d4f0a04ddb5066a7b82b520b62e534a5f476c
chiumax <dumblole@gmail.com>
2023-05-26 21:04:56 -0400

    take util from statsbar

--- src/typescript/frontend/src/components/trade/DepositWithdrawModal/SelectMarketContent.tsx
+++ src/typescript/frontend/src/components/trade/DepositWithdrawModal/SelectMarketContent.tsx
@@ -115,6 +115,16 @@ export const SelectMarketContent: React.FC<{
   );
 };
 
+// copy paste from statsbar, think about making a unified component later
+const DEFAULT_TOKEN_ICON = "/tokenImages/default.png";
+type MarketIconPairProps = {
+  baseAssetIcon?: string;
+  quoteAssetIcon?: string;
+};
+const MarketIconPair = ({
+  baseAssetIcon = DEFAULT_TOKEN_ICON,
+  quoteAssetIcon = DEFAULT_TOKEN_ICON,
+}: MarketIconPairProps) => {
   // TODO: add this so statsbar can use it too
   interface ImageWithFallbackProps {
     fallback?: string;
@@ -144,3 +154,44 @@ export const SelectMarketContent: React.FC<{
       />
     );
   };
+
+  return (
+    <div className="relative flex">
+      {/* height width props required */}
+      <ImageWithFallback
+        src={baseAssetIcon}
+        alt="market-icon-pair"
+        width={40}
+        height={40}
+        className="z-20 aspect-square  w-[30px] min-w-[30px] md:min-w-[40px]"
+      ></ImageWithFallback>
+      <ImageWithFallback
+        src={quoteAssetIcon}
+        alt="market-icon-pair"
+        width={40}
+        height={40}
+        className="absolute z-10 aspect-square w-[30px] min-w-[30px] translate-x-1/2 md:min-w-[40px]"
+      ></ImageWithFallback>
+    </div>
+  );
+};
+
+const getLang = () => {
+  return typeof window === "undefined"
+    ? "en"
+    : navigator.language || navigator.languages[0];
+};
+
+const formatNumber = (num: number | undefined, digits: number): string => {
+  if (!num) return "-";
+  return num.toLocaleString(getLang(), {
+    minimumFractionDigits: digits,
+    maximumFractionDigits: digits,
+  });
+};
+
+const plusMinus = (num: number | undefined): string => {
+  if (!num) return "";
+  // no need to return - as numbers will already have that
+  return num >= 0 ? `+` : ``;
+};

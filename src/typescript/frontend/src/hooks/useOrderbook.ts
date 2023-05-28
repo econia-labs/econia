45f72731bebad22e809bb563b6e4672eec874fe6
chiumax <dumblole@gmail.com>
2023-05-28 19:05:21 -0400

    update market id naming

--- src/typescript/frontend/src/hooks/useOrderbook.ts
+++ src/typescript/frontend/src/hooks/useOrderbook.ts
@@ -2,15 +2,15 @@ import { useQuery, type UseQueryResult } from "@tanstack/react-query";
 import { API_URL } from "@/env";
 // TODO: precision not yet implemented in API yet, so does nothing as of now
 export const useOrderBook = (
-  market: number,
+  market_id: number,
   precision = "0",
   depth = 60
 ): UseQueryResult<OrderBook> => {
   return useQuery(
-    ["orderBook", market, precision],
+    ["orderBook", market_id, precision],
     async () => {
       const response = await fetch(
-        `${API_URL}/market/${market}/orderbook?depth=${depth}`
+        `${API_URL}/market/${market_id}/orderbook?depth=${depth}`
       );
       const data = await response.json();
       return data as OrderBook;

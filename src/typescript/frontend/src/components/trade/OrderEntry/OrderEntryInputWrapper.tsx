f2f6fb861bf7c91f4412d2fca5d5c76b60cfc93e
chiumax <dumblole@gmail.com>
2023-06-02 20:12:38 -0400

    optional pass custom ref

--- src/typescript/frontend/src/components/trade/OrderEntry/OrderEntryInputWrapper.tsx
+++ src/typescript/frontend/src/components/trade/OrderEntry/OrderEntryInputWrapper.tsx
@@ -1,14 +1,30 @@
-import { type PropsWithChildren } from "react";
+import {
+  ReactElement,
+  type PropsWithChildren,
+  useRef,
+  MutableRefObject,
+  ReactNode,
+} from "react";
 
 export const OrderEntryInputWrapper: React.FC<
   PropsWithChildren<{
     startAdornment?: string;
     endAdornment?: string;
     labelFor?: string;
+    inputWithRef?: (
+      ref: MutableRefObject<null | HTMLInputElement>
+    ) => ReactNode;
   }>
-> = ({ startAdornment, endAdornment, labelFor, children }) => {
+> = ({ startAdornment, endAdornment, labelFor, children, inputWithRef }) => {
+  const ref = useRef<null | HTMLInputElement>(null);
+  const handleFocus = () => {
+    ref.current?.focus();
+  };
   return (
-    <div className="flex h-10 w-full items-baseline gap-2 border border-neutral-600 px-4 py-2">
+    <div
+      className="flex h-10 w-full items-baseline gap-2 border border-neutral-600 px-4 py-2"
+      onClick={handleFocus}
+    >
       {/* start adornment */}
       <label
         htmlFor={labelFor}
@@ -16,7 +32,8 @@ export const OrderEntryInputWrapper: React.FC<
       >
         {startAdornment}
       </label>
-      {children}
+      {inputWithRef ? inputWithRef(ref) : children}
+      {/* {children} */}
       {/* end adornment */}
       <span className="flex h-full items-center font-roboto-mono text-xs font-light text-neutral-400">
         {endAdornment}

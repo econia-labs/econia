c3cef980afb43efc932cf680f95c04ab569e4045
chiumax <dumblole@gmail.com>
2023-06-06 21:18:38 -0400

    consistent limit order sizing

--- src/typescript/frontend/src/components/trade/OrderEntry/OrderEntryInputWrapper.tsx
+++ src/typescript/frontend/src/components/trade/OrderEntry/OrderEntryInputWrapper.tsx
@@ -12,13 +12,13 @@ export const OrderEntryInputWrapper: React.FC<
       {/* start adornment */}
       <label
         htmlFor={labelFor}
-        className="flex h-full items-center whitespace-nowrap font-roboto-mono text-xs text-white"
+        className="flex h-full min-w-[80px] items-center whitespace-nowrap font-roboto-mono text-xs text-white"
       >
         {startAdornment}
       </label>
       {children}
       {/* end adornment */}
-      <span className="flex h-full items-center font-roboto-mono text-xs font-light text-neutral-400">
+      <span className="flex h-full min-w-[37px] items-center font-roboto-mono text-xs font-light text-neutral-400">
         {endAdornment}
       </span>
     </div>

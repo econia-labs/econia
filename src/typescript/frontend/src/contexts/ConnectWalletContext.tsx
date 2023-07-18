1b031557cf156f03caa1fd3c8ec9b1e5929a17e7
bogberry <bogberry@users.noreply.github.com>
2023-07-18 23:04:50 +0900

    catch error on wallet connect

--- src/typescript/frontend/src/contexts/ConnectWalletContext.tsx
+++ src/typescript/frontend/src/contexts/ConnectWalletContext.tsx
@@ -8,6 +8,7 @@ import {
 
 import { BaseModal } from "@/components/BaseModal";
 import { ArrowIcon } from "@/components/icons/ArrowIcon";
+import { toast } from "react-toastify";
 
 export type ConnectWalletContextState = {
   connectWallet: () => void;
@@ -45,8 +46,15 @@ export function ConnectWalletContextProvider({ children }: PropsWithChildren) {
               key={wallet.name}
               className="relative flex w-full cursor-pointer items-center gap-2 border border-neutral-600 p-4 font-jost text-lg font-medium  text-neutral-500 transition-all hover:border-blue [&:hover>#arrow-wrapper]:border-blue [&:hover>#arrow-wrapper]:bg-blue [&:hover>#token-icon]:border-blue [&:hover>div>#arrow]:rotate-[-45deg]"
               onClick={() => {
-                connect(wallet.name);
-                setOpen(false);
+                try {
+                  connect(wallet.name);
+                } catch (e) {
+                  if (e instanceof Error) {
+                    toast.error(e.message);
+                  }
+                } finally {
+                  setOpen(false);
+                }
               }}
             >
               <img

7001fbc9fbeeaea0366526c3cf80459017556ec0
bogberry <bogberry@users.noreply.github.com>
2023-07-20 15:34:14 +0900

    hide depth chart when screen height is not tall enough

--- src/typescript/frontend/src/components/DepthChart.tsx
+++ src/typescript/frontend/src/components/DepthChart.tsx
@@ -122,7 +122,7 @@ export const DepthChart: React.FC<{
   }, [marketData, baseCoinInfo, quoteCoinInfo, data, isFetching]);
 
   return (
-    <div className="relative h-1/5 flex-[1_1_0%]">
+    <>
       <p className={"absolute ml-4 mt-2 font-jost text-white"}>Depth</p>
       <div
         className={
@@ -271,7 +271,7 @@ export const DepthChart: React.FC<{
           }}
         />
       </div>
-    </div>
+    </>
   );
 };
 

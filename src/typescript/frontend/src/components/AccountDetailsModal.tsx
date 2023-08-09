4eca1d66aecc41f2d895124a200c687806f45a11
chiumax <dumblole@gmail.com>
2023-08-09 13:16:56 -0700

    mock content

--- src/typescript/frontend/src/components/AccountDetailsModal.tsx
+++ src/typescript/frontend/src/components/AccountDetailsModal.tsx
@@ -1,15 +1,20 @@
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import {
+  ArrowRightOnRectangleIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/20/solid";
 import React from "react";
 
 import { Button } from "@/components/Button";
 import { ECONIA_ADDR } from "@/env";
 import { type ApiMarket } from "@/types/api";
-import { ImageWithFallback } from "./MarketIconPair";
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
+
+import { MarketIconPair } from "./MarketIconPair";
 
 export const AccountDetailsModal: React.FC<{
   selectedMarket: ApiMarket;
 }> = ({ selectedMarket }) => {
-  const { disconnect } = useWallet();
+  const { account } = useWallet();
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(ECONIA_ADDR);
@@ -20,15 +25,20 @@ export const AccountDetailsModal: React.FC<{
         <p className="font-jost text-3xl font-bold text-white">
           Account Details
         </p>
-        <div>
-          {/* card */}
+        {/* card */}
+        <div className={"flex"}>
           <div>
-            <div className="border-[1px_solid_var(--neutral-colors-600, #565656)] text-white">
-              dropdown copy button
+            <div className="border-[1px] border-neutral-600 text-white">
+              {shorten(account?.address)}
             </div>
-            <Button variant="primary" onClick={() => {}}>
-              Disconnect
-            </Button>{" "}
+            <Button
+              variant="secondary"
+              onClick={() => {}}
+              className={"flex items-center"}
+            >
+              Disconnect{" "}
+              <ArrowRightOnRectangleIcon className="ml-2 inline-block h-4 w-4 text-center" />
+            </Button>
           </div>
           <div>
             <div className="ml-8 text-left md:block">
@@ -52,9 +62,10 @@ export const AccountDetailsModal: React.FC<{
         <p className="font-jost text-3xl font-bold text-white">
           Market Accounts
         </p>
+        <DepositWithdrawCard />
       </div>
       <div>
-        <Button variant="primary" onClick={() => {}}>
+        <Button variant="secondary" onClick={() => {}}>
           Add New Account
         </Button>
       </div>
@@ -63,47 +74,48 @@ export const AccountDetailsModal: React.FC<{
 };
 
 const DepositWithdrawCard: React.FC = () => {
+  const [expanded, setExpanded] = React.useState(false);
+  const toggleExpanded = () => setExpanded(!expanded);
   return (
-    <div>
+    <div className="flex text-white">
       {/* card */}
       {/* left column */}
       <div>
-        <div>dropdown</div>
-        <Button variant="primary" onClick={() => {}}>
-          Add New Account
+        <div>
+          <MarketIconPair /> APT/USDC <span>verified icon</span>
+        </div>
+        <div className="text-neutral-500">
+          LAYERZERO
+          <ChevronDownIcon className="inline-block h-4 w-4 text-center" />
+        </div>
+        <Button variant="secondary" onClick={() => {}}>
+          Deposit / Withdraw
         </Button>{" "}
       </div>
       {/* right column */}
       <div>
         {/* right column first row BASE balance */}
         <div>
-          <div>
-            <ImageWithFallback
-              src={""}
-              alt="market-icon-pair"
-              width={28}
-              height={28}
-              className="z-20 aspect-square w-7"
-            ></ImageWithFallback>
+          <div className="font-roboto-mono text-xs font-light text-neutral-500">
             APT BALANCE
           </div>
-          <div>111.00</div>
+          <div className="inline-block text-white">111.00</div>
         </div>
         {/* right column second row QUOTE balance*/}
         <div>
-          <div>
-            <ImageWithFallback
-              src={""}
-              alt="market-icon-pair"
-              width={28}
-              height={28}
-              className="z-20 aspect-square w-7"
-            ></ImageWithFallback>{" "}
+          <div className="font-roboto-mono text-xs font-light text-neutral-500">
             APT BALANCE
           </div>
-          <div>111.00</div>
+          <div className="inline-block text-white">111.00</div>
         </div>
       </div>
     </div>
   );
 };
+
+// generated
+function shorten(str: string | undefined, maxLen = 10, separator = "") {
+  if (str == undefined) return "";
+  if (str.length <= maxLen) return str;
+  return str.substr(0, str.lastIndexOf(separator, maxLen)) + "..";
+}

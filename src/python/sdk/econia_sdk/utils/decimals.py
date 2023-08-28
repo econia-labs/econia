38213ef01294c98f0f1eedad7a086ba222e2a204
Elliott Dehnbostel <elliott@econialabs.com>
2023-08-28 16:15:26 -0400

    formatting and comments

--- src/python/sdk/econia_sdk/utils/decimals.py
+++ src/python/sdk/econia_sdk/utils/decimals.py
@@ -3,6 +3,9 @@ from decimal import Decimal
 from typing import Tuple
 
 
+MAX_TICKS_INTEGER = (2**32) - 1
+
+
 def _verify_decimal_input(decimal_input: str):
     if type(decimal_input) is not str:
         raise ValueError("Decimal input should be a string")
@@ -31,9 +34,11 @@ def _get_tick_size_integer(
     _verify_decimal_input(size_precision_nominal)
     _verify_decimal_input(price_precision_nominal)
     _verify_integer_input(quote_decimals)
-    tick_size = Decimal(price_precision_nominal) \
-      * Decimal(size_precision_nominal) \
-      * (10 ** quote_decimals)
+    tick_size = (
+        Decimal(price_precision_nominal)
+        * Decimal(size_precision_nominal)
+        * (10**quote_decimals)
+    )
     if tick_size < 1:
         raise ValueError("The price is too granular given the size granularity")
     return math.ceil(tick_size)
@@ -52,8 +57,8 @@ def get_market_parameters_integer(
     can be directly used to register a market (along with the base/quote types).
 
     Parameters:
-    * `lot_size_nominal`: The size granularity in nominal base terms.
-    * `tick_size_nominal`: The tick granularity in nominal quote terms.
+    * `size_precision_nominal`: The size precision in nominal base terms.
+    * `price_precision_nominal`: The price precision in nominal quote/base terms.
     * `min_size_nominal`: The minimum size in nominal base terms.
     * `base_decimals`: The number of decimals in the nominal base unit.
     * `quote_decimals`: The number of decimals in the nominal quote unit.
@@ -62,9 +67,7 @@ def get_market_parameters_integer(
     """
     lot_size = _get_lot_size_integer(size_precision_nominal, base_decimals)
     tick_size = _get_tick_size_integer(
-        size_precision_nominal,
-        price_precision_nominal,
-        quote_decimals
+        size_precision_nominal, price_precision_nominal, quote_decimals
     )
     min_size = _get_min_size_integer(min_size_nominal, base_decimals, lot_size)
     return (lot_size, tick_size, min_size)
@@ -95,9 +98,11 @@ def get_market_parameters_nominal(
     _verify_integer_input(min_size_integer)
     _verify_integer_input(base_decimals)
     _verify_integer_input(quote_decimals)
-    lot_size_nominal = Decimal(lot_size_integer) / (10 ** base_decimals)
-    tick_size_nominal = Decimal(tick_size_integer) / (10 ** quote_decimals)
-    min_size_nominal = Decimal(min_size_integer * lot_size_integer) / (10 ** base_decimals)
+    lot_size_nominal = Decimal(lot_size_integer) / (10**base_decimals)
+    tick_size_nominal = Decimal(tick_size_integer) / (10**quote_decimals)
+    min_size_nominal = Decimal(min_size_integer * lot_size_integer) / (
+        10**base_decimals
+    )
     return (lot_size_nominal, tick_size_nominal, min_size_nominal)
 
 
@@ -107,14 +112,14 @@ def get_price_integer(
     tick_size_integer: int,
     base_decimals: int,
     quote_decimals: int,
-  ) -> int:
+) -> int:
     """
     Returns the "ticks per lot" price given a nominal price and other configuration variables.
 
     Parameters:
     * `nominal_price`: The price of one nominal unit of base in terms of nominal quote units.
-    * `lot_size`: The subunits of base that make up one lot.
-    * `tick_size`: The subunits of quote that make up one tick.
+    * `lot_size_integer`: The subunits of base that make up one lot.
+    * `tick_size_integer`: The subunits of quote that make up one tick.
     * `base_decimals`: The number of decimals in one nominal unit of base.
     * `quote_decimals`: The number of decimals in one nominal unit of quote.
     """
@@ -123,13 +128,18 @@ def get_price_integer(
     _verify_integer_input(tick_size_integer)
     _verify_integer_input(base_decimals)
     _verify_integer_input(quote_decimals)
-    result = Decimal(nominal_price) * Decimal((10 ** quote_decimals) / (10 ** base_decimals))
+    result = Decimal(nominal_price) * Decimal(
+        (10**quote_decimals) / (10**base_decimals)
+    )
     result = result * Decimal((1 / tick_size_integer) / (1 / lot_size_integer))
     result_floor = math.floor(result)
     result_ceil = math.ceil(result)
-    if (result_floor != result_ceil):
-        raise ValueError("The price is not expressible as an integer with the parameters")
+    if result_floor != result_ceil:
+        raise ValueError(
+            "The price is not expressible as an integer with the parameters"
+        )
     return result_ceil
+
 
 def get_price_nominal(
     integer_price: int,
@@ -143,16 +153,18 @@ def get_price_nominal(
 
     Parameters:
     * `integer_price`: The number of ticks of quote per lot representing the price.
-    * `lot_size`: The subunits of base that make up one lot.
-    * `tick_size`: The subunits of quote that make up one tick.
+    * `lot_size_integer`: The subunits of base that make up one lot.
+    * `tick_size_integer`: The subunits of quote that make up one tick.
+    * `base_decimals`: The number of decimals in one nominal unit of base.
+    * `quote_decimals`: The number of decimals in one nominal unit of quote.
     """
     _verify_integer_input(integer_price)
     _verify_integer_input(lot_size_integer)
     _verify_integer_input(tick_size_integer)
     _verify_integer_input(base_decimals)
     _verify_integer_input(quote_decimals)
-    quote_units = Decimal(integer_price * tick_size_integer) / (10 ** quote_decimals)
-    base_units = Decimal(lot_size_integer) / (10 ** base_decimals)
+    quote_units = Decimal(integer_price * tick_size_integer) / (10**quote_decimals)
+    base_units = Decimal(lot_size_integer) / (10**base_decimals)
     return quote_units / base_units
 
 
@@ -176,10 +188,7 @@ def get_max_price_nominal(price_precision_nominal: str) -> Decimal:
     can be thought of as maximum price.
 
     Parameters:
-    * `smallest_decimal_size_base`: The decimal size of one lot of base,
-      as a string i.e "0.001" for one-thousandth.
-    * `smallest_decimal_size_quote`: The decimal size of one tick of quote,
-      as a string i.e "0.001" for one-thousandth.
+    * `price_precision_nominal`: The price precision in nominal quote/base terms.
     """
     _verify_decimal_input(price_precision_nominal)
-    return Decimal(price_precision_nominal) * ((2**32)-1)
+    return Decimal(price_precision_nominal) * MAX_TICKS_INTEGER

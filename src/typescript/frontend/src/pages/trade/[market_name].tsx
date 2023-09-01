0d18abfe9cb0f3aab5c0875859480f4e94f3e124
chiumax <dumblole@gmail.com>
2023-08-31 20:42:16 -0400

    fix websocket connect issue

--- src/typescript/frontend/src/pages/trade/[market_name].tsx
+++ src/typescript/frontend/src/pages/trade/[market_name].tsx
@@ -56,9 +56,16 @@ export default function Market({ allMarketData, marketData }: Props) {
   useEffect(() => {
     ws.current = new WebSocket(WS_URL);
     ws.current.onopen = () => {
-      if (marketData?.market_id == null || ws.current == null) {
+      // because useEffects can fire more than once and onopen is an async function, we still want to check readystate when we send a message
+      if (
+        marketData?.market_id == null ||
+        ws.current == null ||
+        ws.current.readyState !== WebSocket.OPEN
+      ) {
         return;
       }
+
+      console.log(ws.current.readyState, "state");
 
       // Subscribe to orderbook price level updates
       ws.current.send(
@@ -82,19 +89,26 @@ export default function Market({ allMarketData, marketData }: Props) {
 
   // Handle wallet connect and disconnect
   useEffect(() => {
-    if (marketData?.market_id == null || ws.current == null) {
+    if (
+      marketData?.market_id == null ||
+      ws.current == null ||
+      ws.current.readyState !== WebSocket.OPEN
+    ) {
       return;
     }
     if (account?.address != null) {
+      //  commenting this out because it doesn't seem to be doing what it's supposed to
+      //  maybe if we made it synchronous it would work?
+
       // If the WebSocket connection is not ready,
       // wait for the WebSocket connection to be opened.
-      if (ws.current.readyState === WebSocket.CONNECTING) {
-        const interval = setInterval(() => {
-          if (ws.current?.readyState === WebSocket.OPEN) {
-            clearInterval(interval);
-          }
-        }, 500);
-      }
+      // if (ws.current.readyState === WebSocket.CONNECTING) {
+      //   const interval = setInterval(() => {
+      //     if (ws.current?.readyState === WebSocket.OPEN) {
+      //       clearInterval(interval);
+      //     }
+      //   }, 500);
+      // }
 
       // Subscribe to orders by account channel
       ws.current.send(
@@ -152,7 +166,7 @@ export default function Market({ allMarketData, marketData }: Props) {
         prevAddress.current = undefined;
       }
     }
-  }, [marketData?.market_id, account?.address]);
+  }, [marketData?.market_id, account?.address, ws.current?.readyState]);
 
   // Handle incoming WebSocket messages
   useEffect(() => {

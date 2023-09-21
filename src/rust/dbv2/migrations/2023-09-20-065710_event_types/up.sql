2c3eb3ca14d4d64fc9d1c24fb2f47f4327e12e4f
Bogdan Crisan <bogdan@crbl.studio>
2023-09-21 12:27:19 +0200

    add swap types

--- src/rust/dbv2/migrations/2023-09-20-065710_event_types/up.sql
+++ src/rust/dbv2/migrations/2023-09-20-065710_event_types/up.sql
@@ -29,3 +29,22 @@ CREATE TABLE
     size NUMERIC(20) NOT NULL,
     self_match_behavior SMALLINT NOT NULL
   );
+
+
+CREATE TABLE
+  place_swap_order_events (
+    txn_version NUMERIC(20) NOT NULL,
+    event_idx NUMERIC(20) NOT NULL,
+    PRIMARY KEY (txn_version, event_idx),
+    market_id NUMERIC(20) NOT NULL,
+    TIME timestamptz NOT NULL,
+    order_id NUMERIC(39) NOT NULL,
+    signing_account VARCHAR(70) NOT NULL,
+    integrator VARCHAR(70) NOT NULL,
+    direction BOOLEAN NOT NULL,
+    min_base NUMERIC(20) NOT NULL,
+    max_base NUMERIC(20) NOT NULL,
+    min_quote NUMERIC(20) NOT NULL,
+    max_quote NUMERIC(20) NOT NULL,
+    limit_price NUMERIC(20) NOT NULL
+  );

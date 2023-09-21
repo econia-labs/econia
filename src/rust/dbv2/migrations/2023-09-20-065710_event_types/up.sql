315079118e95ac46a46f59d8a5622844a8462357
Bogdan Crisan <bogdan@crbl.studio>
2023-09-21 12:17:42 +0200

    fix sql errors

--- src/rust/dbv2/migrations/2023-09-20-065710_event_types/up.sql
+++ src/rust/dbv2/migrations/2023-09-20-065710_event_types/up.sql
@@ -7,7 +7,7 @@ CREATE TABLE
     market_id NUMERIC(20) NOT NULL,
     TIME timestamptz NOT NULL,
     order_id NUMERIC(39) NOT NULL,
-    USER VARCHAR(70) NOT NULL,
+    "user" VARCHAR(70) NOT NULL,
     custodian_id NUMERIC(20) NOT NULL,
     side BOOLEAN NOT NULL,
     new_size NUMERIC(20) NOT NULL
@@ -22,10 +22,10 @@ CREATE TABLE
     market_id NUMERIC(20) NOT NULL,
     TIME timestamptz NOT NULL,
     order_id NUMERIC(39) NOT NULL,
-    USER VARCHAR(70) NOT NULL,
+    "user" VARCHAR(70) NOT NULL,
     custodian_id NUMERIC(20) NOT NULL,
     integrator VARCHAR(70) NOT NULL,
     direction BOOLEAN NOT NULL,
     size NUMERIC(20) NOT NULL,
-    self_match_behavior SMALLINT NOT NULL,
+    self_match_behavior SMALLINT NOT NULL
   );

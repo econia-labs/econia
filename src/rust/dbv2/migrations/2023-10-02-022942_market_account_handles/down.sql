ceaccc0746b2c7fd292a019736733c8e5ffe5cdd
alnoki <43892045+alnoki@users.noreply.github.com>
2023-10-02 14:49:08 -0700

    Update submodule, clear migrations

--- src/rust/dbv2/migrations/2023-10-02-022942_market_account_handles/down.sql
+++
@@ -1,9 +0,0 @@
--- Table drop generated via diesel migration generate --diff-schema.
--- View drops generated manually.
-DROP TABLE IF EXISTS "market_account_handles";
-DROP VIEW api.market_account_handles;
-
--- Old view drops that should've been in prior migrations.
-DROP VIEW api.change_order_size_events;
-DROP VIEW api.place_market_order_events;
-DROP VIEW api.place_swap_order_events;

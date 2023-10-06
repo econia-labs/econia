259e39406c7adcf281fca7f2bd4be1cb1ad5ba75
Bogdan Crisan <bogdan@crbl.studio>
2023-10-06 18:34:52 +0200

    use ask and bid instead of true and false

--- src/rust/dbv2/migrations/2023-10-03-070930_add_order_queue/up.sql
+++ src/rust/dbv2/migrations/2023-10-03-070930_add_order_queue/up.sql
@@ -6,7 +6,25 @@ ALTER TABLE aggregator.user_history_limit ADD COLUMN price NUMERIC(20) NOT NULL;
 ALTER TABLE aggregator.user_history_limit ADD COLUMN last_increase_stamp NUMERIC NOT NULL;
 
 CREATE VIEW api.limit_orders AS
-    SELECT * FROM aggregator.user_history_limit NATURAL JOIN aggregator.user_history;
+    SELECT
+        market_id,
+        order_id,
+        user,
+        custodian_id,
+        self_matching_behavior,
+        restriction,
+        created_at,
+        last_updated_at,
+        integrator,
+        total_filled,
+        remaining_size,
+        order_status,
+        order_type,
+        CASE
+            WHEN side = true THEN 'ask'
+            ELSE 'bid'
+        END AS side
+    FROM aggregator.user_history_limit NATURAL JOIN aggregator.user_history;
 
 GRANT SELECT ON api.limit_orders TO web_anon;
 

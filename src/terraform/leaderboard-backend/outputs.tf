b820c9ea8421dcc3fa726a47b64dbd44bca147f8
alnoki <43892045+alnoki@users.noreply.github.com>
2023-10-17 09:59:11 -0700

    Sort outputs

--- src/terraform/leaderboard-backend/outputs.tf
+++ src/terraform/leaderboard-backend/outputs.tf
@@ -2,10 +2,10 @@ output "db_conn_str_admin" {
   value = local.db_conn_str_admin
 }
 
-output "postgres_public_ip" {
-  value = local.postgres_public_ip
-}
-
 output "db_conn_str_private" {
   value = local.db_conn_str_private
+}
+
+output "postgres_public_ip" {
+  value = local.postgres_public_ip
 }

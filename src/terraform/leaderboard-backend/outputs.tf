fa57209ac681d7fd5f0a3c7703c84db35adbf6a9
alnoki <43892045+alnoki@users.noreply.github.com>
2023-10-17 20:18:03 -0700

    Simplify outputs

--- src/terraform/leaderboard-backend/outputs.tf
+++ src/terraform/leaderboard-backend/outputs.tf
@@ -2,14 +2,6 @@ output "db_conn_str_admin" {
   value = local.db_conn_str_admin
 }
 
-output "db_conn_str_private" {
-  value = local.db_conn_str_private
-}
-
-output "postgres_public_ip" {
-  value = local.postgres_public_ip
-}
-
 output "postgrest_url" {
   value = google_cloud_run_v2_service.postgrest.uri
 }

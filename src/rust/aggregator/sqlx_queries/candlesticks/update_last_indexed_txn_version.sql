8616fa64d8e6af17228260dd5331e78cdb9da6ad
Bogdan Crisan <bogdan@crbl.studio>
2023-11-01 21:58:01 +0100

    fix max(txn_version) being null issue

--- src/rust/aggregator/sqlx_queries/candlesticks/update_last_indexed_txn_version.sql
+++ src/rust/aggregator/sqlx_queries/candlesticks/update_last_indexed_txn_version.sql
@@ -3,7 +3,7 @@ WITH parameters AS (
         $1::int AS resolution)
 UPDATE aggregator.candlesticks_last_indexed_txn
 SET
-    txn_version = (SELECT MAX(txn_version) FROM fill_events)
+    txn_version = COALESCE((SELECT (MAX(txn_version)) FROM fill_events), txn_version)
 FROM
     parameters
 WHERE candlesticks_last_indexed_txn.resolution = parameters.resolution

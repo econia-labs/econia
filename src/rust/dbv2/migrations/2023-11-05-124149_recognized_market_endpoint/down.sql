7cba639ad16984466bd36d0cc13befecf4adce1b
Bogdan Crisan <bogdan@crbl.studio>
2023-11-05 14:07:42 +0100

    create `markets` and `recognized_markets` endpoints

---
+++ src/rust/dbv2/migrations/2023-11-05-124149_recognized_market_endpoint/down.sql
@@ -0,0 +1,5 @@
+-- This file should undo anything in `up.sql`
+DROP VIEW api.markets;
+
+
+DROP VIEW api.recognized_markets;
---
+++ src/rust/dbv2/migrations/2023-11-05-124149_recognized_market_endpoint/up.sql
@@ -0,0 +1,58 @@
+-- Your SQL goes here
+CREATE VIEW api.recognized_markets AS
+    SELECT
+    DISTINCT ON (
+        base_account_address,
+        base_module_name,
+        base_struct_name,
+        base_name_generic,
+        quote_account_address,
+        quote_module_name,
+        quote_struct_name
+    )
+        *
+    FROM
+        recognized_market_events
+    ORDER BY
+        base_account_address,
+        base_module_name,
+        base_struct_name,
+        base_name_generic,
+        quote_account_address,
+        quote_module_name,
+        quote_struct_name,
+        txn_version DESC,
+        event_idx DESC;
+
+
+GRANT SELECT ON api.recognized_markets TO web_anon;
+
+
+CREATE VIEW api.markets AS
+    SELECT
+        m.*,
+        CASE
+            WHEN r.market_id = m.market_id THEN true
+            ELSE false
+        END AS is_recognized
+    FROM
+        market_registration_events AS m
+    LEFT JOIN
+        api.recognized_markets AS r
+    ON
+        COALESCE(r.base_account_address, '') = COALESCE(m.base_account_address, '')
+    AND
+        COALESCE(r.base_module_name, '') = COALESCE(m.base_module_name, '')
+    AND
+        COALESCE(r.base_struct_name, '') = COALESCE(m.base_struct_name, '')
+    AND
+        COALESCE(r.base_name_generic, '') = COALESCE(m.base_name_generic, '')
+    AND
+        r.quote_account_address = m.quote_account_address
+    AND
+        r.quote_module_name = m.quote_module_name
+    AND
+        r.quote_struct_name = m.quote_struct_name;
+
+
+GRANT SELECT ON api.markets TO web_anon;

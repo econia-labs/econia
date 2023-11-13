e0e0a64754f9152c73f83552029dcc15b71696fb
Bogdan Crisan <bogdan@crbl.studio>
2023-11-13 20:08:06 +0100

    organize up.sql

--- src/rust/dbv2/migrations/2023-11-10-132429_optimizations/up.sql
+++ src/rust/dbv2/migrations/2023-11-10-132429_optimizations/up.sql
@@ -1,4 +1,12 @@
 -- Your SQL goes here
+
+--  ___                     _    _   _         _
+-- |   \ _ _ ___ _ __   ___| |__| | (_)_ _  __| |_____ _____ ___
+-- | |) | '_/ _ \ '_ \ / _ \ / _` | | | ' \/ _` / -_) \ / -_|_-<
+-- |___/|_| \___/ .__/ \___/_\__,_| |_|_||_\__,_\___/_\_\___/__/
+--              |_|
+
+
 DROP INDEX timeprice;
 DROP INDEX txnv_fills;
 DROP INDEX txnv_limit;
@@ -11,43 +19,68 @@ DROP INDEX aggregator.price_levels;
 DROP INDEX aggregator.ranking_idx;
 
 
+--    _      _    _          _      _                         _    _    _
+--   /_\  __| |__| |  __ ___| |___ (_)_ _    _  _ ___ ___ _ _| |_ (_)__| |_ ___ _ _ _  _
+--  / _ \/ _` / _` | / _/ _ \ (_-< | | ' \  | || (_-</ -_) '_| ' \| (_-<  _/ _ \ '_| || |
+-- /_/ \_\__,_\__,_| \__\___/_/__/ |_|_||_|  \_,_/__/\___|_|_|_||_|_/__/\__\___/_|  \_, |
+
+
 CREATE TYPE order_direction AS ENUM('sell', 'buy', 'ask', 'bid');
+
 
 ALTER TABLE aggregator.user_history
 ADD COLUMN "user" text;
 
+
 ALTER TABLE aggregator.user_history
 ADD COLUMN direction order_direction;
+
 
 ALTER TABLE aggregator.user_history
 ADD COLUMN price numeric(20,0);
 
+
 ALTER TABLE aggregator.user_history
 ADD COLUMN average_execution_price numeric(20,0);
+
 
 ALTER TABLE aggregator.user_history
 ADD COLUMN custodian_id numeric(20,0);
 
+
 ALTER TABLE aggregator.user_history
 ADD COLUMN self_match_behavior smallint;
+
 
 ALTER TABLE aggregator.user_history
 ADD COLUMN restriction smallint;
 
+
 ALTER TABLE aggregator.user_history
 ADD COLUMN last_increase_stamp numeric(39,0);
+
 
 ALTER TABLE aggregator.user_history
 ADD COLUMN min_base numeric(20,0);
 
+
 ALTER TABLE aggregator.user_history
 ADD COLUMN max_base numeric(20,0);
+
 
 ALTER TABLE aggregator.user_history
 ADD COLUMN min_quote numeric(20,0);
 
+
 ALTER TABLE aggregator.user_history
 ADD COLUMN max_quote numeric(20,0);
+
+
+--  ___ _ _ _                            _
+-- | __(_) | |  _ _  _____ __ __  __ ___| |___
+-- | _|| | | | | ' \/ -_) V  V / / _/ _ \ (_-<
+-- |_| |_|_|_| |_||_\___|\_/\_/  \__\___/_/__/
+
 
 UPDATE aggregator.user_history AS u
 SET
@@ -67,6 +100,7 @@ SET
 FROM aggregator.user_history_limit AS l
 WHERE l.market_id = u.market_id AND l.order_id = u.order_id;
 
+
 UPDATE aggregator.user_history AS u
 SET
     "user" = m."user",
@@ -85,6 +119,7 @@ SET
 FROM aggregator.user_history_market AS m
 WHERE m.market_id = u.market_id AND m.order_id = u.order_id;
 
+
 UPDATE aggregator.user_history AS u
 SET
     "user" = s.signing_account,
@@ -102,6 +137,7 @@ SET
     max_quote = s.max_quote
 FROM aggregator.user_history_swap AS s
 WHERE s.market_id = u.market_id AND s.order_id = u.order_id;
+
 
 WITH deduped_fills AS (
     SELECT
@@ -145,19 +181,52 @@ SET
 FROM avg_price AS f
 WHERE f.market_id = u.market_id AND f.order_id = u.order_id;
 
+
+--  ___                     _    _       _     _        _
+-- |   \ _ _ ___ _ __   ___| |__| |  ___| |__ (_)___ __| |_ ___
+-- | |) | '_/ _ \ '_ \ / _ \ / _` | / _ \ '_ \| / -_) _|  _(_-<
+-- |___/|_| \___/ .__/ \___/_\__,_| \___/_.__// \___\__|\__/__/
+--              |_|                         |__/
+
+
 DROP VIEW api.limit_orders;
 DROP VIEW api.market_orders;
 DROP VIEW api.swap_orders;
+DROP VIEW api.orders;
+
 
 DROP VIEW api.price_levels;
 
+
 DROP FUNCTION api.average_execution_price;
 
-DROP VIEW api.orders;
 
 DROP TABLE aggregator.user_history_limit;
 DROP TABLE aggregator.user_history_market;
 DROP TABLE aggregator.user_history_swap;
+
+
+DROP TRIGGER updated_order_trigger ON aggregator.user_history;
+DROP FUNCTION notify_updated_order;
+
+
+--   ___              _                               _     _        _
+--  / __|_ _ ___ __ _| |_ ___   _ _  _____ __ __  ___| |__ (_)___ __| |_ ___
+-- | (__| '_/ -_) _` |  _/ -_) | ' \/ -_) V  V / / _ \ '_ \| / -_) _|  _(_-<
+--  \___|_| \___\__,_|\__\___| |_||_\___|\_/\_/  \___/_.__// \___\__|\__/__/
+--                                                       |__/
+
+
+CREATE FUNCTION notify_updated_order () RETURNS TRIGGER AS $$
+BEGIN
+    PERFORM pg_notify('econiaws', json_build_object('channel', 'updated_limit_order', 'payload', NEW)::text);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER updated_order_trigger
+AFTER UPDATE ON aggregator.user_history FOR EACH ROW
+EXECUTE PROCEDURE notify_updated_order ();
 
 
 CREATE VIEW api.price_levels AS
@@ -166,9 +235,7 @@ CREATE VIEW api.price_levels AS
         direction,
         price,
         SUM(remaining_size) AS total_size,
-        (SELECT txn_version FROM aggregator.user_history_last_indexed_txn LIMIT 1) AS version
-    FROM
-        aggregator.user_history
+        (SELECT txn_version FROM aggregator.user_history_last_indexed_txn LIMIT 1) AS version FROM aggregator.user_history
     WHERE
         order_status = 'open'
     GROUP BY
@@ -185,29 +252,19 @@ SELECT
   ON api.price_levels TO web_anon;
 
 
-DROP TRIGGER updated_order_trigger ON aggregator.user_history;
-DROP FUNCTION notify_updated_order;
-
-CREATE FUNCTION notify_updated_order () RETURNS TRIGGER AS $$
-BEGIN
-    PERFORM pg_notify('econiaws', json_build_object('channel', 'updated_limit_order', 'payload', NEW)::text);
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER updated_order_trigger
-AFTER UPDATE ON aggregator.user_history FOR EACH ROW
-EXECUTE PROCEDURE notify_updated_order ();
-
-
 CREATE or replace VIEW api.orders AS
     SELECT
         *
     FROM
         aggregator.user_history;
 
-
 GRANT SELECT ON api.orders TO web_anon;
+
+
+--   ___              _                           _         _
+--  / __|_ _ ___ __ _| |_ ___   _ _  _____ __ __ (_)_ _  __| |_____ _____ ___
+-- | (__| '_/ -_) _` |  _/ -_) | ' \/ -_) V  V / | | ' \/ _` / -_) \ / -_|_-<
+--  \___|_| \___\__,_|\__\___| |_||_\___|\_/\_/  |_|_||_\__,_\___/_\_\___/__/
 
 
 CREATE INDEX fill_events_time ON fill_events ("time");

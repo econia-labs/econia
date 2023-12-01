8798ab354e1f8de1e1045b7a483b635aa00fd78a
Bogdan Crisan <bogdan@crbl.studio>
2023-12-01 20:45:44 +0100

    add healthcheck endpoint

---
+++ src/rust/dbv2/migrations/2023-12-01-180017_healthcheck/up.sql
@@ -0,0 +1,42 @@
+-- Your SQL goes here
+CREATE or replace FUNCTION api.healthcheck ()
+RETURNS boolean AS $$
+    SELECT (
+        SELECT (
+            SELECT SUM(total_filled)
+            FROM api.orders
+        ) = (
+            SELECT SUM(size)
+            FROM fill_events
+            WHERE txn_version <= (
+                SELECT txn_version
+                FROM aggregator.user_history_last_indexed_txn
+            )
+        )
+    ) AND (
+        WITH last_candlesticks AS (
+            SELECT DISTINCT ON (market_id, resolution)
+                volume,
+                start_time,
+                resolution,
+                market_id
+            FROM api.candlesticks
+            ORDER BY market_id, resolution, start_time DESC
+        )
+        SELECT
+            SUM(CASE WHEN ok THEN 0 ELSE 1 END) = 0
+        FROM (
+            SELECT SUM(size*price)/2 = volume AS ok
+            FROM fill_events, last_candlesticks
+            WHERE txn_version <= (
+                SELECT txn_version
+                FROM aggregator.candlesticks_last_indexed_txn AS i
+                WHERE i.resolution = last_candlesticks.resolution
+            )
+            AND fill_events.market_id = last_candlesticks.market_id
+            AND "time" >= start_time
+            AND "time" < start_time + '1 second'::interval * resolution
+            GROUP BY volume, last_candlesticks.market_id, resolution
+        ) AS lool
+    );
+$$ LANGUAGE SQL;

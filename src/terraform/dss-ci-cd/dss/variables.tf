ed0024f4f46274920001c66e2f55bc8e2d943898
alnoki <43892045+alnoki@users.noreply.github.com>
2023-12-11 17:17:34 -0800

    Get Terraform proj initialized on runner

--- src/terraform/dss-ci-cd/dss/variables.tf
+++ src/terraform/dss-ci-cd/dss/variables.tf
@@ -9,3 +9,13 @@ variable "region" {
 variable "zone" {
   default = "us-central1-c"
 }
+
+variable "aptos_network" {}
+
+variable "econia_address" {}
+
+variable "starting_version" {}
+
+variable "grpc_data_service_address" {}
+
+variable "grpc_auth_token" {}
--- src/terraform/dss-ci-cd/runner/scripts/startup-script.sh
+++ src/terraform/dss-ci-cd/runner/scripts/startup-script.sh
@@ -4,34 +4,38 @@
 mkdir /app
 cd /app
 
+# Install dependencies.
+# https://developer.hashicorp.com/terraform/install
+# https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/install-cli
+apt update && apt install -y \
+    git \
+    gnupg \
+    software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg |
+    gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" |
+    tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install -y terraform
+
 # Get service account key.
 SERVICE_ACCOUNT_NAME=terraform@$PROJECT_ID.iam.gserviceaccount.com
 gcloud iam service-accounts keys create $CREDENTIALS_FILE \
     --iam-account $SERVICE_ACCOUNT_NAME
 
-# Store DSS variables.
+# Initialize Terraform project.
+git clone \
+    https://github.com/econia-labs/econia.git \
+    --branch ECO-1018 \
+    --recurse-submodules
+cp -R econia/src/terraform/dss-ci-cd/dss/* .
 echo "\
 credentials_file = \"$CREDENTIALS_FILE\"
-project_id = \"$PROJECT_ID\"
+project = \"$PROJECT_ID\"
 aptos_network = \"$APTOS_NETWORK\"
 econia_address = \"$ECONIA_ADDRESS\"
 starting_version = \"$STARTING_VERSION\"
 grpc_data_service_address = \"$GRPC_DATA_SERVICE_ADDRESS\"
 grpc_auth_token = \"$GRPC_AUTH_TOKEN\"" >terraform.tfvars
-
-# Install Git and Terraform.
-# https://developer.hashicorp.com/terraform/install
-# https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/install-cli
-sudo apt-get update && apt-get install -y \
-    git \
-    gnupg \
-    software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg |
-    sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" |
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
-
-# Get Econia.
-git clone https://github.com/econia-labs/econia.git --recurse-submodules
+terraform fmt
+terraform init

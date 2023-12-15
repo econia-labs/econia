efc967114053b0342239a7f63719411b30001bad
Bogdan Crisan <bogdan@crbl.studio>
2023-12-15 20:27:07 +0100

    make processor easly restartable

--- src/docker/processor/start.sh
+++ src/docker/processor/start.sh
@@ -13,4 +13,10 @@ if [[ "$HEALTHCHECK_BEFORE_START" == "true" ]];then
     done
 fi
 
+psql $DATABASE_URL -c '\copy processor_status to out.csv csv'
+
+if [ -s "out.csv" ];then
+    sed -i "s/starting_version: *[0-9]\+/starting_version: $(cut -d, -f2 out.csv)/g" /config/data/config.yaml
+fi
+
 /usr/local/bin/processor -c /config/data/config.yaml

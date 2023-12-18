c67814391b39cda71f61495ce08f1d9fd3ecb1db
alnoki <43892045+alnoki@users.noreply.github.com>
2023-12-18 14:30:08 -0800

    Make DB URLs with embedded passwords sensitive

--- src/terraform/dss-ci-cd/dss/outputs.tf
+++ src/terraform/dss-ci-cd/dss/outputs.tf
@@ -3,10 +3,12 @@ output "db_connection_name" {
 }
 
 output "db_conn_str_auth_proxy" {
+  sensitive = true
   value = module.db.db_conn_str_auth_proxy
 }
 
 output "db_conn_str_private" {
+  sensitive = true
   value = module.db.db_conn_str_private
 }
 

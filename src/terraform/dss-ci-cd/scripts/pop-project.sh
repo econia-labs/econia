015dd294cc4f894ac1adb91724362df25b20930c
alnoki <43892045+alnoki@users.noreply.github.com>
2023-12-18 15:39:18 -0800

    Update config to use tfvars for popping project

--- src/terraform/dss-ci-cd/scripts/pop-project.sh
+++ src/terraform/dss-ci-cd/scripts/pop-project.sh
@@ -1,7 +1,4 @@
 PROJECT_ID=$1
-gcloud config set project $PROJECT_ID
-gcloud config set compute/zone us-central1-c
-gcloud config set run/region us-central1
 STASH_DIR="stashed-projects/$PROJECT_ID"
 source scripts/clear-project-state-except-tfvars.sh
 mv "$STASH_DIR/terraform.tfstate" "runner/terraform.tfstate"
@@ -9,3 +6,10 @@ mv "$STASH_DIR/terraform.tfvars" "runner/terraform.tfvars"
 mv "$STASH_DIR/service-account-key.json" "runner/service-account-key.json"
 terraform -chdir=runner init
 rm -rf $STASH_DIR
+source scripts/get-tfvar.sh
+TFVARS=$(cat runner/terraform.tfvars)
+REGION=$(get_tfvar region $TFVARS)
+ZONE=$(get_tfvar zone $TFVARS)
+gcloud config set project $PROJECT_ID
+gcloud config set compute/zone $ZONE
+gcloud config set run/region $REGION

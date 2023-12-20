84aa68f11d404e5c80cde0c673c5a9f63defd320
alnoki <43892045+alnoki@users.noreply.github.com>
2023-12-20 14:20:57 -0800

    Fix typo in kill command

--- src/terraform/dss-ci-cd/dss/modules/db/run-migrations.sh
+++ src/terraform/dss-ci-cd/dss/modules/db/run-migrations.sh
@@ -3,4 +3,4 @@ cloud-sql-proxy $DB_CONNECTION_NAME --credentials-file $CREDENTIALS_FILE &
 sleep 5 # Give proxy time to start up.
 diesel migration run --migration-dir migrations
 # https://unix.stackexchange.com/a/104825
-kii $(pgrep cloud-sql-proxy)
+kill $(pgrep cloud-sql-proxy)

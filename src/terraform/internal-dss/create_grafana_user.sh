6dfd231d2fdd2f4483cd38b78db2e6d78a9ef917
Bogdan Crisan <bogdan@crbl.studio>
2024-01-08 13:57:01 +0100

    fix create_grafana_user.sh according to pr comments

--- src/terraform/internal-dss/create_grafana_user.sh
+++ src/terraform/internal-dss/create_grafana_user.sh
@@ -2,6 +2,7 @@
 
 # Create a PostgreSQL data source
 curl "http://admin:$ADMIN_PASSWORD@$GRAFANA_HOST:$GRAFANA_PORT/api/datasources" \
+    -X POST \
     -H 'Accept: application/json' \
     -H 'Content-Type: application/json' \
     --data "{\"name\":\"DSS Database\",\"type\":\"postgres\",\"url\":\"$DATABASE_HOST:$DATABASE_PORT\",\"access\":\"proxy\",\"user\":\"$DATABASE_USER\",\"password\":\"$DATABASE_PASSWORD\",\"jsonData\": {\"database\":\"$DATABASE_DB\",\"sslmode\":\"disable\",\"postgresVersion\":1500}}"
@@ -11,4 +12,4 @@ curl "http://admin:$ADMIN_PASSWORD@$GRAFANA_HOST:$GRAFANA_PORT/api/admin/users" 
     -X POST \
     -H 'Accept: application/json' \
     -H 'Content-Type: application/json' \
-    --data "{\"name\":\"public\",\"email\":\"$PUBLIC_USER_EMAIL\",\"login\":\"public\",\"password\":\"$PUBLIC_USER_PASSWORD\",\"OrgId\":1}"
+    --data "{\"name\":\"public\",\"login\":\"public\",\"password\":\"$PUBLIC_USER_PASSWORD\"}"

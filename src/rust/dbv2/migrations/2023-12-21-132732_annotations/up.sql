0ee54bb740c4908f0c724cd565ebebeb0bdd6a80
Bogdan Crisan <bogdan@crbl.studio>
2024-01-08 14:20:28 +0100

    use csv and docker to insert annotations instead of sql

--- src/rust/dbv2/migrations/2023-12-21-132732_annotations/up.sql
+++ src/rust/dbv2/migrations/2023-12-21-132732_annotations/up.sql
@@ -5,28 +5,5 @@ CREATE TABLE annotations (
     "title" text NOT NULL,
     "text" text,
     "tag" text NOT NULL,
-);
-
-INSERT INTO annotations ("time", "title", "tag") VALUES (
-    '2023-11-28 14:03:00+00'::timestamptz,
-    'Econia mainnet launch',
-    'Econia'
-);
-
-INSERT INTO annotations ("time", "title", "tag") VALUES (
-    '2023-11-30 22:03:00+00'::timestamptz,
-    'APT-LZUSDC promo tweet',
-    'Econia'
-);
-
-INSERT INTO annotations ("time", "title", "tag") VALUES (
-    '2023-12-11 15:33:00+00'::timestamptz,
-    'Aries Spotlight article',
-    'Econia'
-);
-
-INSERT INTO annotations ("time", "title", "tag") VALUES (
-    '2023-12-18 13:53:00+00'::timestamptz,
-    'LZeth launch',
-    'Econia'
+    PRIMARY KEY ("title", "tag")
 );

f544c09e607d5751d6d376ab232f67dc0260ad92
Bogdan Crisan <bogdan@crbl.studio>
2024-01-10 19:26:37 +0100

    fix check_db.sh script

--- src/rust/dbv2/check_db.sh
+++ src/rust/dbv2/check_db.sh
@@ -4,13 +4,13 @@ echo "Starting a dedicated PostgreSQL Docker instance"
 
 docker run -d --rm --name econia_db_test -e POSTGRES_PASSWORD=pgpw -p 37949:5432 postgres > /dev/null
 
-DATABASE_URL=postgres://postgres:pgpw@localhost:37949/econia
+TEST_DATABASE_URL=postgres://postgres:pgpw@localhost:37949/econia
 
 echo "Waiting 5 seconds for the database to start..." && sleep 5
 
 echo "Running migrations back and forth"
 
-if diesel database setup && diesel migration redo --all; then
+if diesel database setup --database-url $TEST_DATABASE_URL && diesel migration redo --database-url $TEST_DATABASE_URL --all; then
     echo "All good !"
 else
     echo "An error occured."

c211e721e9071bdf22fda04625a3f8d67fb14088
Bogdan Crisan <bogdan@crbl.studio>
2024-01-11 16:18:13 +0100

    fix price divisor calculation

--- src/rust/dbv2/migrations/2024-01-09-160715_price_to_nominal/up.sql
+++ src/rust/dbv2/migrations/2024-01-09-160715_price_to_nominal/up.sql
@@ -1,10 +1,12 @@
 -- Your SQL goes here
 CREATE FUNCTION get_price_divisor_for_market(numeric) RETURNS NUMERIC IMMUTABLE AS $$
     SELECT COALESCE ((
-        SELECT POW(10,decimals) / lot_size
+        SELECT 1 / ((tick_size / POW(10,quote.decimals)) / (lot_size / POW(10,base.decimals)))
         FROM market_registration_events
-        INNER JOIN aggregator.coins
+        INNER JOIN aggregator.coins base
         ON base_account_address = address AND base_module_name = module AND base_struct_name = struct
+        INNER JOIN aggregator.coins quote
+        ON quote_account_address = address AND quote_module_name = module AND quote_struct_name = struct
         WHERE market_id = $1
         AND base_name_generic IS NULL
     ),1);

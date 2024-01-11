a4a15275ad9d4ea5d02d5e5b98a3a41e7627721e
Bogdan Crisan <bogdan@crbl.studio>
2024-01-11 22:17:16 +0100

    add more utility functions

--- src/rust/dbv2/migrations/2024-01-09-160715_price_to_nominal/up.sql
+++ src/rust/dbv2/migrations/2024-01-09-160715_price_to_nominal/up.sql
@@ -1,14 +1,35 @@
 -- Your SQL goes here
-CREATE FUNCTION get_price_divisor_for_market(numeric) RETURNS NUMERIC IMMUTABLE AS $$
-    SELECT COALESCE ((
-        SELECT 1 / ((tick_size / POW(10,quote.decimals)) / (lot_size / POW(10,base.decimals)))
-        FROM market_registration_events
-        INNER JOIN aggregator.coins base
-        ON base_account_address = address AND base_module_name = module AND base_struct_name = struct
-        INNER JOIN aggregator.coins quote
-        ON quote_account_address = address AND quote_module_name = module AND quote_struct_name = struct
-        WHERE market_id = $1
-        AND base_name_generic IS NULL
-    ),1);
+CREATE FUNCTION integer_price_to_quote_indivisible_subunits(market_id numeric, price numeric) RETURNS NUMERIC IMMUTABLE AS $$
+    SELECT ($2 / ((tick_size / POW(10::numeric,quote.decimals)) / (lot_size / POW(10,COALESCE(base.decimals, 0)))))
+    FROM market_registration_events
+    LEFT JOIN aggregator.coins base
+    ON base_account_address = base."address" AND base_module_name = base.module AND base_struct_name = base.struct
+    INNER JOIN aggregator.coins quote
+    ON quote_account_address = quote."address" AND quote_module_name = quote.module AND quote_struct_name = quote.struct
+    WHERE market_id = $1;
+$$ LANGUAGE sql;
+
+CREATE FUNCTION quote_indivisible_subunits_to_integer_price(market_id numeric, price numeric) RETURNS NUMERIC IMMUTABLE AS $$
+    SELECT $2 * ((tick_size / POW(10::numeric,quote.decimals)) / (lot_size / POW(10,COALESCE(base.decimals, 0))))
+    FROM market_registration_events
+    LEFT JOIN aggregator.coins base
+    ON base_account_address = base."address" AND base_module_name = base.module AND base_struct_name = base.struct
+    INNER JOIN aggregator.coins quote
+    ON quote_account_address = quote."address" AND quote_module_name = quote.module AND quote_struct_name = quote.struct
+    WHERE market_id = $1;
+$$ LANGUAGE sql;
+
+
+CREATE FUNCTION size_to_base_indivisible_subunits(market_id numeric, "size" numeric) RETURNS NUMERIC IMMUTABLE AS $$
+    SELECT $2 * lot_size
+    FROM market_registration_events
+    WHERE market_id = $1;
+$$ LANGUAGE sql;
+
+
+CREATE FUNCTION size_and_price_to_quote_indivisible_subunits(market_id numeric, size numeric, price numeric) RETURNS NUMERIC IMMUTABLE AS $$
+    SELECT $2 * $3 * tick_size
+    FROM market_registration_events
+    WHERE market_id = $1;
 $$ LANGUAGE sql;
 

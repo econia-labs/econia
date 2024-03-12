e888769e813db72ebd3705fb6434301d1ca06e3b
Bogdan Crisan <bogdan@crbl.studio>
2024-03-12 17:40:45 +0100

    remove duped doc from event.py

--- src/python/sdk/examples/event.py
+++ src/python/sdk/examples/event.py
@@ -5,27 +5,6 @@ import json
 
 # This is an example on how to use subscribe to events emitted by the DSS.
 
-# Once you have a DSS running (see
-# https://econia.dev/off-chain/dss/data-service-stack on how to do that), you
-# can run this script by running:
-#
-# $ poetry install
-# $ poetry run event
-#
-# It will subscribe to all fill events and to place limit order events on
-# market 7.
-#
-# Each MQTT message has a topic and a payload. You can subscribe to topics.
-# When specifying the topic you want to subscribe to, you can put a + instead
-# of a value to subscribe to all events matching the rest of the topic.
-#
-# Example:
-#
-# We use the topic fill/MARKET_ID/USER_ADDRESS/CUSTODIAN_ID. Fill events by the
-# user 0xdeadbeef with custodian ID 1 on market 3 will have fill/3/0xdeadbeef/1
-# as a topic. But you could subscribe to fill/+/0xdeadbeef/+ to get all fill
-# events from the user 0xdeadbeef.
-
 HOST = "127.0.0.1"
 PORT = 21883
 

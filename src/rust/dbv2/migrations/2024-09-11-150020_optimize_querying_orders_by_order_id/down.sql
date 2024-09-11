628c725d1901a76bf1197ab2b943b007931ea597
Bogdan Crisan <bogdan@crbl.studio>
2024-09-11 17:01:39 +0200

    Add index to speed up querying orders by order_id

---
+++ src/rust/dbv2/migrations/2024-09-11-150020_optimize_querying_orders_by_order_id/down.sql
@@ -0,0 +1,2 @@
+-- This file should undo anything in `up.sql`
+DROP INDEX aggregator.user_history_order_id;
---
+++ src/rust/dbv2/migrations/2024-09-11-150020_optimize_querying_orders_by_order_id/up.sql
@@ -0,0 +1,2 @@
+-- Your SQL goes here
+CREATE INDEX user_history_order_id ON aggregator.user_history (order_id);

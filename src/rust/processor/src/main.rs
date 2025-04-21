db17d50f4a4bf242f4d5137c270b0aa67c73d57b
Bogdan Crisan <bogdan@crbl.studio>
2025-04-21 12:06:12 +0200

    Use mutex lock to avoid concurrency issues

--- src/rust/processor/src/main.rs
+++ src/rust/processor/src/main.rs
@@ -1,6 +1,9 @@
+use std::sync::LazyLock;
+
 use aptos_indexer_processor_sdk::postgres::basic_processor::process;
 use diesel_migrations::{embed_migrations, EmbeddedMigrations};
 use processor::process_transactions;
+use tokio::sync::Mutex;
 
 mod events;
 mod processor;
@@ -8,12 +11,15 @@ mod processor;
 // This is just a dummy to pass to the process functions, no migrations will be run
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
+static LOCK: LazyLock<Mutex<()>> = LazyLock::new(|| Mutex::new(()));
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     process(
         "econia".to_string(),
         MIGRATIONS,
         async |transactions, conn_pool| {
+            let _ = LOCK.lock().await;
             process_transactions(transactions, conn_pool.clone()).await.unwrap();
             Ok(())
         },
